7cbea2ef1f6aaa06b30155703407048ab784ec1a
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-06-13 17:55:07 +0000

    compilation warning fixed

--- 4sum.cc
+++ 4sum.cc
@@ -1,4 +1,21 @@
 // 4Sum
 class Solution {
 public:
-  vector<vector
+  vector<vector<int> > fourSum(vector<int> &a, int target) {
+    int n = a.size(), old;
+    multimap<int, int> m;
+    vector<vector<int>> r;
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; ) {
+      // a <= b < c <= d
+      for (int j = i+1; j < n; ) {
+        int t = target-a[i]-a[j];
+        auto it = m.equal_range(t);
+        for (; it.first != it.second; ++it.first) {
+          vector<int> b{it.first->second, t-it.first->second, a[i], a[j]};
+          r.push_back(b);
+        }
+        old = a[j];
+        while (++j < n && a[j] == old);
+      }
+      /
