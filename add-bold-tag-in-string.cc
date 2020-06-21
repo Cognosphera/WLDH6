b75941650672275f662f33a082c3f8b7aa4eb318
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-06-21 05:27:22 +0000

    adjust css

--- add-bold-tag-in-string.cc
+++ add-bold-tag-in-string.cc
@@ -9,4 +9,25 @@ public:
         while (j && t[j] != t[i]) j = pi[j-1];
         if (t[j] == t[i]) j++;
         pi[i] = j;
- 
+      }
+      for (int j = 0, i = 0; i < s.size(); i++) {
+        while (j && t[j] != s[i]) j = pi[j-1];
+        if (t[j] == s[i] && ++j == t.size()) {
+          a[i-t.size()+1]++;
+          a[i+1]--;
+          j = pi[j-1];
+        }
+      }
+    }
+    string r;
+    int c = 0, cc;
+    for (int i = 0; i <= s.size(); i++) {
+      cc = c+a[i];
+      if (! c && cc) r += "<b>";
+      else if (c && ! cc) r += "</b>";
+      if (i < n) r += s[i];
+      c = cc;
+    }
+    return r;
+  }
+};
---
+++ add-digits.cc
@@ -0,0 +1,5 @@
+// Add Digits
+class Solution {
+public:
+  int addDigits(int num) {
+    return num ? num%9 ? num
