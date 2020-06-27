51b836f9478c6df3fb5c4e0373e9162f2069a6fa
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-06-27 17:16:57 +0000

    doc: update CHANGELOG

--- all-oone-data-structure.cc
+++ all-oone-data-structure.cc
@@ -6,4 +6,24 @@ class AllOne {
 public:
   /** Initialize your data structure here. */
   AllOne() {
-    b.push_fron
+    b.push_front({0, {}});
+  }
+
+  /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+  void inc(string key) {
+    if (! a.count(key)) {
+      a[key] = b.begin();
+      b.begin()->ks.insert(key);
+    }
+    auto y = a[key], x = y++;
+    if (y == b.end() || x->v+1 < y->v)
+      y = b.insert(y, {x->v+1, {}});
+    a[key] = y;
+    y->ks.insert(key);
+    x->ks.erase(key);
+    if (x->ks.empty() && x->v)
+      b.erase(x);
+  }
+
+  /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
+  void dec(string
