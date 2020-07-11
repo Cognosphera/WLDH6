b92cd7a7f794cc333a9aa4e413b9e43e06deebb4
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-07-11 09:18:58 +0000

    Update Vietnamese translation

--- basic-calculator-ii.cc
+++ basic-calculator-ii.cc
@@ -11,4 +11,22 @@ public:
     icp['\0'] = 0; icp['+'] = 2; icp['-'] = 2; icp['*'] = 4; icp['/'] = 4; icp['('] = 6; icp[')'] = 1;
     ops.push('\0');
     for (size_t i = 0; i <= s.size(); ) {
-      char op = i == s.size() ? '\0' : s[i
+      char op = i == s.size() ? '\0' : s[i];
+      if (op == ' ')
+        i++;
+      else if (isdigit(op)) {
+        int x = 0;
+        do x = x*10+s[i]-'0';
+        while (isdigit(s[++i]));
+        xs.push(x); // shift
+      } else {
+        i++;
+        int x, y;
+        for (; isp[ops.top()] > icp[op]; ops.pop()) {
+          y = xs.top(); xs.pop();
+          x = xs.top(); xs.pop();
+          switch (ops.top()) {
+          case '+': xs.push(x+y); break;
+          case '-': xs.push(x-y); break;
+          case '*': xs.push(x*y); break;
+          case '
