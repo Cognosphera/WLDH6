bca290d8bd3ab9ec25da2d6b3412694d404e3877
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-07-25 08:14:21 +0000

    [ADD] addons table in README.md

--- binary-search-tree-iterator.cc
+++ binary-search-tree-iterator.cc
@@ -1,4 +1,35 @@
 // Binary Search Tree Iterator
 class BSTIterator {
 public:
-  stack<T
+  stack<TreeNode *> s;
+
+  BSTIterator(TreeNode *t) {
+    for (; t; t = t->left)
+      s.push(t);
+  }
+
+  /** @return whether we have a next smallest number */
+  bool hasNext() {
+    return ! s.empty();
+  }
+
+  /** @return the next smallest number */
+  int next() {
+    TreeNode *x = s.top();
+    s.pop();
+    int r = x->val;
+    for (x = x->right; x; x = x->left)
+      s.push(x);
+    return r;
+  }
+};
+
+// Morris inorder traversal
+
+class BSTIterator {
+public:
+  TreeNode *x;
+
+  BSTIterator(TreeNode *t) : x(t) {}
+
+  /** @return whether
