0aee05659b1ed29d6e93e38222184234941aba28
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-02 13:49:29 +0000

    intelli_plug: fix incorrect cpufreq API usage

--- binary-tree-maximum-path-sum.nim
+++ binary-tree-maximum-path-sum.nim
@@ -3,4 +3,14 @@ type
     val: cint
     left, right: ptr TreeNode
 
-proc maxPathSum(root: ptr TreeNode): int 
+proc maxPathSum(root: ptr TreeNode): int {.exportc.} =
+  var res: int = root.val
+  proc dfs(x: ptr TreeNode): int =
+    if x == nil: return 0
+    let l = max(dfs(x.left), 0)
+    let r = max(dfs(x.right), 0)
+    res = max(res, l+x.val+r)
+    max(l, r)+x.val
+
+  discard dfs(root)
+  res
