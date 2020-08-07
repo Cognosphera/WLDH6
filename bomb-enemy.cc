edd469278f77fa2b976e7af2b0b6fa4fdd8a7df6
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-07 21:19:11 +0000

    Improve code

--- bomb-enemy.cc
+++ bomb-enemy.cc
@@ -7,4 +7,20 @@ public:
   int maxKilledEnemies(vector<vector<char>>& grid) {
     int m = grid.size(), n = m ? grid[0].size() : 0, r = 0, row, col[n];
     REP(i, m)
-      REP(j, n) 
+      REP(j, n) {
+        if (! j || grid[i][j-1] == 'W') {
+          row = 0;
+          for (int k = j; k < n && grid[i][k] != 'W'; k++)
+            row += grid[i][k] == 'E';
+        }
+        if (! i || grid[i-1][j] == 'W') {
+          col[j] = 0;
+          for (int k = i; k < m && grid[k][j] != 'W'; k++)
+            col[j] += grid[k][j] == 'E';
+        }
+        if (grid[i][j] == '0')
+          r = max(r, row+col[j]);
+      }
+    return r;
+  }
+};
---
+++ bricks-falling-when-hit.cc
@@ -0,0 +1,2 @@
+// Bricks Falling When Hit
+#define FOR(i, a, b) for (remove_cv<remove_reference<declty
