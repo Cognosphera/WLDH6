e03ec904e1af3b47176afc03f00fbbbfbeebb81b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-09 10:49:18 +0000

    change type

--- candy.cc
+++ candy.cc
@@ -5,4 +5,10 @@ public:
     vector<int> a(ratings.size(), 1);
     for (int i = 1; i < ratings.size(); i++)
       if (ratings[i-1] < ratings[i])
-        a[i] = a[i-1]+
+        a[i] = a[i-1]+1;
+    for (int i = ratings.size()-2; i >= 0; i--)
+      if (ratings[i] > ratings[i+1])
+        a[i] = max(a[i], a[i+1]+1);
+    return accumulate(a.begin(), a.end(), 0);
+  }
+};
