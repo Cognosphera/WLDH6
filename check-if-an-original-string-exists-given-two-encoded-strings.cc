e3e661e85f72423651d5f7d7e41df097c3ec9491
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-16 17:09:20 +0000

    Automatic build binary update

--- check-if-an-original-string-exists-given-two-encoded-strings.cc
+++ check-if-an-original-string-exists-given-two-encoded-strings.cc
@@ -1,3 +1,22 @@
 // Check if an Original String Exists Given Two Encoded Strings
 const int P = 32768;
-struct
+struct HNode {int key; bool val; HNode *next; } pool[P*2], *head[P], *allo;
+
+class Solution {
+  static const int BASE = 999*10;
+  string s1, s2;
+  bool f(int i, int j, int p) {
+    int key = ((p+BASE)*41+i)*41+j, hash = key%P;
+    for (HNode *x = head[hash]; x; x = x->next)
+      if (x->key == key)
+        return x->val;
+    if (s1.size() == i && s2.size() == j) return !p;
+    bool ret = false;
+
+    if (unsigned(s1[i]-'0') < 10) {
+      int x = 0;
+      do {
+        x = x*10+s1[i]-'0';
+        ret = f(++i, j, p+x);
+      } while (!ret && unsigned(s1[i]-'0') < 10);
+    } else if
