e13d4bd178a291c73462c5dec438a29ee0b4767e
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-24 13:28:38 +0000

    Remove obsolete code.

--- closest-binary-search-tree-value-ii.cc
+++ closest-binary-search-tree-value-ii.cc
@@ -22,4 +22,20 @@ public:
       auto x = sl.top()->left;
       sl.pop();
       for (; x; x = x->right)
-   
+        sl.push(x);
+      return v;
+    };
+    auto popr = [&]() {
+      int v = sr.top()->val;
+      auto x = sr.top()->right;
+      sr.pop();
+      for (; x; x = x->left)
+        sr.push(x);
+      return v;
+    };
+    vector<int> r;
+    while (r.size() < k)
+      r.push_back(sr.empty() || ! sl.empty() && fabs(sl.top()->val-target) < fabs(sr.top()->val-target) ? popl() : popr());
+    return r;
+  }
+};
---
+++ closest-binary-search-tree-value.cc
@@ -0,0 +1,6 @@
+// Closest Binary Search Tree Value
+class Solution {
+public:
+  int closestValue(TreeNode *x, double target) {
+    int opt;
+    double optd = n
