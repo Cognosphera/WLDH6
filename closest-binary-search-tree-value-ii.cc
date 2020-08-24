05921424ed4c17f03250e11613721e07dee28826
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-24 13:22:14 +0000

    USB: ftdi_sio: add Tagsys RFID Reader IDs

--- closest-binary-search-tree-value-ii.cc
+++ closest-binary-search-tree-value-ii.cc
@@ -1,1 +1,25 @@
-// Clos
+// Closest Binary Search Tree Value II
+class Solution {
+public:
+  vector<int> closestKValues(TreeNode* root, double target, int k) {
+    stack<TreeNode*> sl, sr;
+    TreeNode *x = root;
+    while (x)
+      if (x->val <= target) {
+        sl.push(x);
+        x = x->right;
+      } else
+        x = x->left;
+    x = root;
+    while (x)
+      if (x->val > target) {
+        sr.push(x);
+        x = x->left;
+      } else
+        x = x->right;
+    auto popl = [&]() {
+      int v = sl.top()->val;
+      auto x = sl.top()->left;
+      sl.pop();
+      for (; x; x = x->right)
+   
