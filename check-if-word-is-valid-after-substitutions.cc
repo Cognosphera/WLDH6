e6663fc3af0838470bdd8350ff88247c90fe51b4
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-24 07:15:40 +0000

    fix function names

--- check-if-word-is-valid-after-substitutions.cc
+++ check-if-word-is-valid-after-substitutions.cc
@@ -6,3 +6,11 @@ public:
     for (char c: S)
       if (c != 'c')
         S[n++] = c;
+      else {
+        if (n < 2 || S[n-1] != 'b' || S[n-2] != 'a')
+          return false;
+        n -= 2;
+      }
+    return !n;
+  }
+};
---
+++ clone-graph.cc
@@ -0,0 +1,3 @@
+// Clone Graph
+class Solution {
+pu
