2ae1d61c7d36e56922f7850945d3494861a663b3
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-08-26 11:19:35 +0000

    Added Web Trends

--- closest-room.rs
+++ closest-room.rs
@@ -3,4 +3,18 @@ impl Solution {
     pub fn closest_room(mut rooms: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         use std::collections::BTreeSet;
         rooms.sort_unstable_by_key(|r| -r[1]);
-        let mut queries: Vec<_> = queries.into_iter().enumerat
+        let mut queries: Vec<_> = queries.into_iter().enumerate().collect();
+        queries.sort_unstable_by_key(|(_,q)| -q[1]);
+        let mut tree = BTreeSet::new();
+        let mut ret = vec![-1; queries.len()];
+        let mut j = 0;
+        for (i, q) in queries {
+            while j < rooms.len() && rooms[j][1] >= q[1] {
+                tree.insert(rooms[j][0]);
+                j += 1;
+            }
+            if tree.is_empty() { continue; }
+            if let Some(&x) = tree.range(..q[0]).next_back() {
+                ret[i] = x;
+            }
+            if let Some(&x)
