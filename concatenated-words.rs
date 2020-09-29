82a4f6dbd14f186bf0cbd5b7c35ec7c64f89e2ec
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-09-29 10:01:30 +0000

    Release v0.0.9

--- concatenated-words.rs
+++ concatenated-words.rs
@@ -5,4 +5,17 @@ struct Trie { c: [Option<Box<Self>>; 26], is_word: bool }
 impl Solution {
     pub fn find_all_concatenated_words_in_a_dict(mut words: Vec<String>) -> Vec<String> {
         words.sort_by_key(|w| w.len());
-        
+        let mut trie: Trie = Default::default();
+        let mut vis: Vec<isize> = vec![0; words.last().unwrap().len()+1];
+        let mut q: Vec<usize> = vec![];
+        let mut tick = 0;
+        let mut ans: Vec<String> = vec![];
+        for ww in words.into_iter() {
+            if ww.is_empty() { continue; }
+            let w = ww.as_bytes();
+            q.clear();
+            q.push(0);
+            tick += 1;
+            while let Some(bgn) = q.pop() {
+                let mut t: &Trie = &mut trie;
+                for i in bgn..w.len() {
