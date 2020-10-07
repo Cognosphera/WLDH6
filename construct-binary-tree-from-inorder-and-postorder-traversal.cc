802a5f97671a556b9ff8e2f6011ae824802cd36b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-10-07 20:43:35 +0000

    Add Procfile.

--- construct-binary-tree-from-inorder-and-postorder-traversal.cc
+++ construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -7,4 +7,29 @@ public:
   TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
     if (inorder.empty()) return nullptr;
     stack<TreeNode *> s;
-  
+    TreeNode *x = nullptr;
+    auto i = inorder.begin(), j = postorder.begin();
+    while (j != postorder.end())
+      if (! s.empty() && s.top()->val == *j) {
+        s.top()->right = x;
+        x = s.top();
+        s.pop();
+        ++j;
+      } else {
+        auto y = new TreeNode(*i++);
+        y->left = x;
+        x = nullptr;
+        s.push(y);
+      }
+    return x;
+  }
+};
+
+// recursive
+
+#define FOR(i, a, b) for (decltype(b) i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+
+class Solution {
+public:
+  TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
