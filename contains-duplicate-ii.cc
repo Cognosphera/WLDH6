15ca4616afb6b1fbac90f9362e9c160191ef701e
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-10-15 18:33:04 +0000

    Stupid mistake

--- contains-duplicate-ii.cc
+++ contains-duplicate-ii.cc
@@ -2,4 +2,12 @@
 class Solution {
 public:
   bool containsNearbyDuplicate(vector<int>& a, int k) {
-    map<
+    map<int, int> m;
+    for (size_t i = 0; i < a.size(); i++) {
+      if (m.count(a[i]) && i-m[a[i]] <= k)
+        return true;
+      m[a[i]] = i;
+    }
+    return false;
+  }
+};
---
+++ contains-duplicate-iii.cc
@@ -0,0 +1,10 @@
+// Contains Duplicate III
+class Solution {
+public:
+  bool containsNearbyAlmostDuplicate(vector<int> &a, int k, int t) {
+    typedef long long ll;
+    if (k < 1 || t < 0) return false;
+    unordered_map<ll, ll> b;
+    for (vector<int>::size_type i = 0; i < a.size(); i++) {
+      ll x = a[i], j = (x-INT_MIN)/(t+1LL);
+      if (b.count(j) || b.count(j-1) && x-b[j-1] <= t
