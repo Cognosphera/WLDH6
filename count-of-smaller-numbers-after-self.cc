96bc2bf4c7a4c7929ae79dab0c42e1137886625f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2020-12-28 18:36:10 +0000

    x86, microcode, AMD: Fix broken ucode patch size check

--- count-of-smaller-numbers-after-self.cc
+++ count-of-smaller-numbers-after-self.cc
@@ -1,4 +1,25 @@
 // Count of Smaller Numbers After Self
 // divide and conquer
 typedef pair<int, int> pii;
-#define REP
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+class Solution {
+  vector<pii> a, b;
+  vector<int> c;
+  void f(int l, int r) {
+    if (r-l <= 1)
+      return;
+    int m = l+(r-l)/2;
+    f(l, m);
+    f(m, r);
+    for (int i = l, j = m, k = l; i < m || j < r; )
+      if (j == r || i < m && a[i].first <= a[j].first) {
+        c[a[i].second] += j-m;
+        b[k++] = a[i++];
+      } else
+        b[k++] = a[j++];
+    copy(b.begin()+l, b.begin()+r, a.begin()+l);
+  }
+public:
+  vector<int> countSmaller(vector<int>& nums) {
+    int n = nums
