a8053c718c2cb367cadc353e684f560cbb8aa9ea
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-01-18 09:30:41 +0000

    fixed Readme

--- count-the-digits-that-divide-a-number.nim
+++ count-the-digits-that-divide-a-number.nim
@@ -3,4 +3,7 @@ proc countDigits(num: cint): cint {.exportc.} =
     x = num
     s = 0
   while x > 0:
-    if n
+    if num mod (x mod 10) == 0:
+      s += 1
+    x = x div 10
+  s.cint
---
+++ count-the-repetitions.cc
@@ -0,0 +1,5 @@
+// Count The Repetitions
+class Solution {
+public:
+  int getMaxRepetitions(string s1, int n1, string s2, int n2) {
+    int l1 = s1.size(), l2 = s2.size(), c = 0, lo =
