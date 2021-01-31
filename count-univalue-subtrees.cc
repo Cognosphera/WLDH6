83b777007116259b6ae266bd855cd0a39b91ff6d
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-01-31 19:09:03 +0000

    improve error logging

--- count-univalue-subtrees.cc
+++ count-univalue-subtrees.cc
@@ -7,4 +7,29 @@ class Solution {
     if (! x) return true;
     auto fl = f(x->left, r),
          fr = f(x->right, r);
-    if (fl && fr && (! x->left || x->le
+    if (fl && fr && (! x->left || x->left->val == x->val) &&
+        (! x->right || x->right->val == x->val))
+      return r++, true;
+    return false;
+  }
+public:
+  int countUnivalSubtrees(TreeNode *x) {
+    int r = 0;
+    f(x, r);
+    return r;
+  }
+};
+
+/// post-order traversal with stack
+
+class Solution {
+public:
+  int countUnivalSubtrees(TreeNode *x) {
+    int r = 0;
+    stack<pair<TreeNode*, bool>> s;
+    for(;;) {
+      for (; x; x = x->left)
+        s.push({x, true});
+      while (! s.empty() && s.top().first->right == x) {
+        bool f = s.top().second;
+      
