c36cde1022d4ff865a5d0670b17335f142adaa59
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-02-21 07:24:42 +0000

    developing workchain

--- course-schedule-ii.nim
+++ course-schedule-ii.nim
@@ -9,4 +9,19 @@ proc findOrder(n: int, prereq: ptr UncheckedArray[cintArray], m: int, unused: po
     fore = 0
     rear = 0
   result = cast[cintArray](malloc(cast[csize_t](cint.sizeof * n)))
-  for x in toOpenArray(prer
+  for x in toOpenArray(prereq, 0, m-1):
+    inc d[x[0]]
+    e[x[1]].add x[0]
+  for i in 0..<n:
+    if d[i] == 0:
+      result[rear] = cast[cint](i)
+      inc rear
+  while fore < rear:
+    let x: int = result[fore]
+    inc fore
+    for y in e[x]:
+      dec d[y]
+      if d[y] == 0:
+        result[rear] = cast[cint](y)
+        inc rear
+  returnSize[] = if fore == n: cast[cint](n) else: 0
---
+++ course-schedule.nim
@@ -0,0 +1,3 @@
+type cintArray = ptr UncheckedArray[cint]
+
+proc canFinish(n: int, prerequisites: ptr UncheckedArray[cintArray], m: int, unused: pointer
