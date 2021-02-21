0c420cb81becae8ee59c96a3fb3f4fb539254feb
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-02-21 14:52:48 +0000

    chore(package): update eslint to version 2.0.0

--- create-maximum-number.cc
+++ create-maximum-number.cc
@@ -9,4 +9,33 @@ namespace KoAluru
   int *b;
 
   template<typename T>
-  void bucket
+  void bucket(T a[], int n, int k, bool end)
+  {
+    fill_n(b, k, 0);
+    REP(i, n) b[a[i]]++;
+    if (end)
+      FOR(i, 1, k)
+        b[i] += b[i-1];
+    else {
+      int s = 0;
+      REP(i, k)
+        s += b[i], b[i] = s-b[i];
+    }
+  }
+
+  template<typename T>
+  void plus_to_minus(T a[], int sa[], int n, int k)
+  {
+    bucket(a, n, k, false);
+    sa[b[a[n-1]]++] = n-1;
+    REP(i, n-1) {
+      int j = sa[i]-1;
+      if (j >= 0 && ! t[j])
+        sa[b[a[j]]++] = j;
+    }
+  }
+
+  template<typename T>
+  void minus_to_plus(T a[], int sa[], int n, int k)
+  {
+    bucket(a,
