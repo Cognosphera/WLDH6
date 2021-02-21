e3b9db0745f0f1f947bff193ee60edb2653db574
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-02-21 08:39:20 +0000

    shanchu

---
+++ create-maximum-number.cc
@@ -0,0 +1,12 @@
+// Create Maximum Number
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define ROF(i, a, b) for (int i = (b); --i >= (a); )
+
+namespace KoAluru
+{
+  bool *t;
+  int *b;
+
+  template<typename T>
+  void bucket
