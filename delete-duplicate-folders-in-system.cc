b9ad23b70021ca92acf9e608f60cfe48b3a01a64
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-03-07 17:21:07 +0000

    Add build status to README.

--- delete-duplicate-folders-in-system.cc
+++ delete-duplicate-folders-in-system.cc
@@ -25,3 +25,50 @@ public:
       if (res.second)
         name.try_emplace(tail, path.back());
       for (; cur.size() && (cur.size() > path.size() || cur.size() == path.size() && a[cur.back()] != tail); cur.pop_back()) {
+        a.push_back(0);
+        ranges.emplace_back(cur.back(), a.size());
+      }
+      cur.emplace_back(a.size());
+      a.push_back(tail);
+    }
+    for (; cur.size(); cur.pop_back()) {
+      a.push_back(0);
+      ranges.emplace_back(cur.back(), a.size());
+    }
+
+    long k = code.size()+1, pow = 1;
+    vector<long> h(a.size()+1), pw(a.size());
+    REP(i, a.size()) {
+      pw[i] = pow;
+      h[i+1] = (h[i]+pow*a[i]) % P;
+      pow = pow*k % P;
+    }
+    unordered_map<long, pair<int, int>> seen;
+    cur.assign(a.size()+1, 0);
+    for (auto [l, r]: ranges) {
+      if (r-l == 2) continue;
+      long v = (h[r-1]-h[l+1]+P)*inv(pw[l+1]) % P;
+      auto res = seen.try_emplace(v, pair<int, int>{l, r});
+      if (!res.second) {
+        cur[l]++;
+        cur[r]--;
+        cur[res.first->second.first]++;
+        cur[res.first->second.second]--;
+      }
+    }
+
+    int acc = 0;
+    vector<string> path;
+    vector<vector<string>> ans;
+    REP(i, a.size()) {
+      acc += cur[i];
+      if (a[i]) {
+        path.push_back(name[a[i]]);
+        if (!acc)
+          ans.push_back(path);
+      } else
+        path.pop_back();
+    }
+    return ans;
+  }
+};
---
+++ describe-the-painting.rs
@@ -0,0 +1,3 @@
+// Describe the Painting
+impl Solution {
+    pub fn split_painting(segments: Vec<Vec<i32>>) -> Vec<
