36a5a750375b587496b87335eb1566a5e9895349
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-03-14 18:21:28 +0000

    about me

--- detect-squares.nim
+++ detect-squares.nim
@@ -16,4 +16,17 @@ proc detectSquaresAdd(obj: ptr DetectSquares, point: ptr UncheckedArray[cint], p
   let (x, y) = (cast[int](point[0]), cast[int](point[1]))
   obj.ls.mgetOrPut(x+y, @[]).add(y)
   obj.rs.mgetOrPut(x-y, @[]).add(y)
-  obj.c[
+  obj.c[x+y*N] += 1
+
+proc detectSquaresCount(obj: ptr DetectSquares, point: ptr UncheckedArray[cint], pointSize: cint): int {.exportc.} =
+  let (x0, y0) = (cast[int](point[0]), cast[int](point[1]))
+  for y1 in obj.ls.getOrDefault(x0+y0, @[]):
+    if y0 == y1: continue
+    let x1 = x0+y0-y1
+    result += obj.c[x0+y1*N] * obj.c[x1+y0*N]
+  for y1 in obj.rs.getOrDefault(x0-y0, @[]):
+    if y0 == y1: continue
+    let x1 = x0-y0+y1
+    result += obj.c[x0+y1*N] * obj.c[x1+y0*N]
+
+proc detectSquaresFree(obj: sink DetectSquares) {.exportc.} = discard
