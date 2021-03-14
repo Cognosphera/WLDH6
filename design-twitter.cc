12d582a23bafb858a9dbf677d55cbda2e47a774b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-03-14 13:29:07 +0000

    Fixed readme formatting

--- design-twitter.cc
+++ design-twitter.cc
@@ -9,4 +9,15 @@ public:
 
   }
 
-  /** Compose a new tweet
+  /** Compose a new tweet. */
+  void postTweet(int userId, int tweetId) {
+    tweets[userId].emplace_back(tick++, tweetId);
+  }
+
+  /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+  vector<int> getNewsFeed(int userId) {
+    typedef vector<pair<int, int>>::reverse_iterator it;
+    auto cmp = [](const pair<it, it>& x, const pair<it, it>& y) {
+      return x.first->first < y.first->first;
+    };
+    vector<pair<it, it>>
