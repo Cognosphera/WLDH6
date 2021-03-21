408c916c4dd05ec0103f4d407f39fad7d01675f8
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-03-21 08:21:10 +0000

    prueba2

--- distribute-candies.cc
+++ distribute-candies.cc
@@ -4,4 +4,9 @@
 class Solution {
 public:
   int distributeCandies(vector<int>& candies) {
-    auto r = ca
+    auto r = candies.size()/2;
+    sort(ALL(candies));
+    candies.erase(unique(ALL(candies)), candies.end());
+    return min(r, candies.size());
+  }
+};
---
+++ distribute-repeating-integers.rs
@@ -0,0 +1,4 @@
+// Distribute Repeating Integers
+impl Solution {
+    pub fn can_distribute(mut nums: Vec<i32>, quantity: Vec<i32>) -> bool {
+        nums.so
