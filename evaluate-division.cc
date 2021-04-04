cfa421a3996e49b6c5b2edbfc11c8e16442fff75
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-04-04 15:08:28 +0000

    Regenerate gemspec for version 0.0.5

--- evaluate-division.cc
+++ evaluate-division.cc
@@ -12,4 +12,24 @@ public:
   vector<double> calcEquation(vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries) {
     int i = 0;
     for (auto& x: equations) {
-      adj[x.first].emplace_back(x.second, values[i])
+      adj[x.first].emplace_back(x.second, values[i]);
+      adj[x.second].emplace_back(x.first, 1.0/values[i]);
+      i++;
+    }
+    i = 0;
+    for (auto& x: adj)
+      dfs(x.first, i++, 1);
+    vector<double> r;
+    for (auto& x: queries)
+      if (! val.count(x.first) || ! val.count(x.second))
+        r.push_back(-1);
+      else {
+        auto &u = val[x.first], &v = val[x.second];
+        if (u.first != v.first)
+          r.push_back(-1);
+        else
+          r.push_back(v.second/u.second);
+      }
+    return r;
+  }
+};
