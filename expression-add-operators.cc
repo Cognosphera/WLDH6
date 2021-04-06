c472ff05755a44a966225116c89c95b4bd5bcab3
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-04-06 06:43:58 +0000

    broken links

--- expression-add-operators.cc
+++ expression-add-operators.cc
@@ -16,4 +16,29 @@ class Solution {
     }
     int x = a[k]-'0';
     if (last) // no leading zero
-      forward(k+1, s+a[k], a
+      forward(k+1, s+a[k], add, mul, last*10+x);
+    forward(k+1, s+'*'+a[k], add, mul*last, x);
+    forward(k+1, s+'+'+a[k], sum, 1, x);
+    forward(k+1, s+'-'+a[k], sum, -1, x);
+  }
+public:
+  vector<string> addOperators(string num, int target) {
+    a = num;
+    n = a.size();
+    if (n) {
+      this->target = target;
+      forward(1, string(1, a[0]), 0, 1, a[0]-'0');
+    }
+    return res;
+  }
+};
+
+/// accelerated by meet-in-the-middle, O(4^(n/2)*2^(n/2)) = O(2^(1.5n))
+
+typedef long long ll;
+class Solution {
+  string a;
+  int n, nn, target;
+  vector<multimap<ll, string>> e_plus, e_minus;
+  vector<string> res;
+  void backward(int
