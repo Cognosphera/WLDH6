3b3b489db770a04c8daacadde6d7b5df26f3d9d1
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-04-25 10:08:16 +0000

    change pic

--- find-duplicate-subtrees.cc
+++ find-duplicate-subtrees.cc
@@ -8,4 +8,27 @@ public:
   static const long P = 1000000007;
   vector<int> a;
   vector<long> h, pw;
-  vector<tuple<TreeNod
+  vector<tuple<TreeNode *, int, int>> ranges;
+  long inv(long x) {
+    long s = 1;
+    for (; x > 1; x = P%x)
+      s = s*(P-P/x)%P;
+    return s;
+  }
+  void dfs(TreeNode *x) {
+    int l = a.size();
+    a.push_back(x->val+K2);
+    if (x->left) dfs(x->left);
+    a.push_back(K-1);
+    if (x->right) dfs(x->right);
+    a.push_back(K-1);
+    ranges.emplace_back(x, l, a.size());
+  }
+  vector<TreeNode *> findDuplicateSubtrees(TreeNode *root) {
+    *this = {};
+    dfs(root);
+    h.resize(a.size()+1);
+    pw.resize(a.size());
+    long pow = 1;
+    REP(i, a.size()) {
+      pw[i
