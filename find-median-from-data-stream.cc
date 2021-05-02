8322b99de1326a1b0a2ed207b908bef396e72e4f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-05-02 06:40:30 +0000

    nlp

--- find-median-from-data-stream.cc
+++ find-median-from-data-stream.cc
@@ -7,4 +7,16 @@ public:
   // Adds a number into the data structure.
   void addNum(int num) {
     mx.push(num);
-    mi
+    mi.push(mx.top());
+    mx.pop();
+    if (mx.size() < mi.size()) {
+      mx.push(mi.top());
+      mi.pop();
+    }
+  }
+
+  // Returns the median of current data stream
+  double findMedian() {
+    return mx.size() > mi.size() ? mx.top() : 0.5 * mx.top() + 0.5 * mi.top();
+  }
+};
