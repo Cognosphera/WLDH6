747316d3c0540071b00d7430bd7a61b9b3a4b38d
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-05-08 10:36:34 +0000

    3.14.0

--- find-target-indices-after-sorting-array.cc
+++ find-target-indices-after-sorting-array.cc
@@ -3,4 +3,13 @@
 #define REP(i, n) for (long i = 0; i < (n); i++)
 
 class Solution {
-pub
+public:
+  vector<int> targetIndices(vector<int>& nums, int target) {
+    vector<int> r;
+    sort(ALL(nums));
+    REP(i, nums.size())
+      if (nums[i] == target)
+        r.push_back(i);
+    return r;
+  }
+};
---
+++ find-the-difference.cc
@@ -0,0 +1,7 @@
+// Find the Difference
+class Solution {
+public:
+  char findTheDifference(string s, string t) {
+    long x = 0;
+    for (char i: t) x ^= i;
+    for (char i: s)
