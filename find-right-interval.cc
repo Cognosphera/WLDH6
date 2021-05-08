ed5b869825f45841006f4cbda5b395c2f97d7dde
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-05-08 06:22:23 +0000

    Initial public version

--- find-right-interval.cc
+++ find-right-interval.cc
@@ -3,4 +3,17 @@
 #define REP(i, n) FOR(i, 0, n)
 
 class Solution {
-pu
+public:
+  vector<int> findRightInterval(vector<Interval>& intervals) {
+    vector<int> r;
+    vector<pair<int, int>> a;
+    REP(i, intervals.size())
+      a.emplace_back(intervals[i].start, i);
+    sort(a.begin(), a.end());
+    for (auto& x: intervals) {
+      auto it = lower_bound(a.begin(), a.end(), make_pair(x.end, 0));
+      r.push_back(it == a.end() ? -1 : it->second);
+    }
+    return r;
+  }
+};
---
+++ find-target-indices-after-sorting-array.cc
@@ -0,0 +1,6 @@
+// Find Target Indices After Sorting Array
+#define ALL(x) (x).begin(), (x).end()
+#define REP(i, n) for (long i = 0; i < (n); i++)
+
+class Solution {
+pub
