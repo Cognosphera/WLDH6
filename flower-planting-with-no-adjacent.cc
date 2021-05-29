8df7808fbddba33c29501d0c78f473b7e5a6ab7a
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-05-29 21:35:26 +0000

    USB: cp210x: add ID for RUGGEDCOM USB Serial Console

--- flower-planting-with-no-adjacent.cc
+++ flower-planting-with-no-adjacent.cc
@@ -9,4 +9,25 @@ public:
     }
     for (int i = 0; i < N; i++)
       if (!c[i]) {
-        vector<int>
+        vector<int> q;
+        int u = i;
+        for(;;) {
+          int t[6] = {};
+          for (int v: g[u])
+            if (c[v])
+              t[c[v]] = 1;
+            else
+              c[v] = 5, q.push_back(v);
+          for (int k = 1; ; k++)
+            if (!t[k]) {
+              c[u] = k;
+              break;
+            }
+          if (q.empty()) break;
+          u = q.back();
+          q.pop_back();
+        }
+      }
+    return c;
+  }
+};
---
+++ fraction-addition-and-subtraction.cc
@@ -0,0 +1,5 @@
+// Fraction Addition and Subtraction
+class Solution {
+  int gcd(int a, int b) {
+    int t;
+    while (b)
