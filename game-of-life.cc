896d4ed5701931bbaeb566d372f32abb835550fc
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-06-03 17:35:29 +0000

    pruning

---
+++ game-of-life.cc
@@ -0,0 +1,9 @@
+// Game of Life
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+class Solution {
+public:
+  void gameOfLife(vector<vector<int>> &a) {
+    int n = a.size();
+    if (! n) return;
+  
