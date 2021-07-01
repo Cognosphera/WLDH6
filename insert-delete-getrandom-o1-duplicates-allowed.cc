bd9a34ce1832d170c87a9b7efc4fc857c642ea1b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-01 08:38:19 +0000

    Fix #47

--- insert-delete-getrandom-o1-duplicates-allowed.cc
+++ insert-delete-getrandom-o1-duplicates-allowed.cc
@@ -9,4 +9,28 @@ public:
 
   }
 
-  /** Inserts a value to the collection. Returns true if the collection did not already contain the specified elemen
+  /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+  bool insert(int val) {
+    bool r = ! m.count(val);
+    a.emplace_back(val, m.emplace(val, a.size()));
+    return r;
+  }
+
+  /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+  bool remove(int val) {
+    auto it = m.find(val);
+    if (it == m.end()) return false;
+    (a[it->second] = a.back()).second->second = it->second;
+    m.erase(it);
+    a.pop_back();
+    return true;
+  }
+
+  /** Get a random element from the collection. */
+  int getRandom() {
+    return a[rand()%a.size()].first;
+  }
+};
+
+/**
+ * Your Rando
