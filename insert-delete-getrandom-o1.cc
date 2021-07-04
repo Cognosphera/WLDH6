e729547b9cf21030805c928588085165b3aebc8b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-04 09:42:20 +0000

    Initial data

--- insert-delete-getrandom-o1.cc
+++ insert-delete-getrandom-o1.cc
@@ -31,4 +31,9 @@ public:
 };
 
 /**
- * Your RandomizedSet object will be instantiated a
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
