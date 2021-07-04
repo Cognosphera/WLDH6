83d54f46d26fda18f4e7670a006d4d287ee0a325
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-04 09:27:11 +0000

    Fix readme file

--- insert-delete-getrandom-o1.cc
+++ insert-delete-getrandom-o1.cc
@@ -9,4 +9,26 @@ public:
 
   /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
   bool insert(int val) {
-    if (! m.empl
+    if (! m.emplace(val, a.size()).second) return false;
+    a.push_back(val);
+    return true;
+  }
+
+  /** Removes a value from the set. Returns true if the set contained the specified element. */
+  bool remove(int val) {
+    auto it = m.find(val);
+    if (it == m.end()) return false;
+    m[a[it->second] = a.back()] = it->second;
+    m.erase(it);
+    a.pop_back();
+    return true;
+  }
+
+  /** Get a random element from the set. */
+  int getRandom() {
+    return a[rand()%a.size()];
+  }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated a
