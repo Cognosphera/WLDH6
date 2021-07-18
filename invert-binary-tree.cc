81c2c62f161b0f5c7b044487a0f3a7ff27cfa7b8
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-18 19:12:34 +0000

    update readme [ci skip]

--- invert-binary-tree.cc
+++ invert-binary-tree.cc
@@ -6,4 +6,27 @@ public:
   TreeNode* invertTree(TreeNode* root) {
     if (root) {
       swap(root->left, root->right);
-      invertTree(root->left)
+      invertTree(root->left);
+      invertTree(root->right);
+    }
+    return root;
+  }
+};
+
+// variant of Morris post-order traversal
+class Solution {
+public:
+  TreeNode* invertTree(TreeNode* root) {
+    TreeNode aux(0), *p = &aux;
+    aux.left = root;
+    while (p) {
+      TreeNode *q = p->left;
+      if (q) {
+        while (q->right && q->right != p) q = q->right;
+        if (q->right == p) {
+          for (TreeNode *r = p->left; ; r = r->left) {
+            swap(r->left, r->right);
+            if (r == q) break;
+          }
+          q->left = NULL;
+        } els
