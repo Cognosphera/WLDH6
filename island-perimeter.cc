be3f99a1d730ff9cfdacb59131e5afc9bec869d4
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-25 13:44:11 +0000

    test changes

--- island-perimeter.cc
+++ island-perimeter.cc
@@ -6,4 +6,14 @@ class Solution {
 public:
   int islandPerimeter(vector<vector<int>>& a) {
     int m = a.size(), n = a[0].size(), s = 0;
-    REP(i, m
+    REP(i, m)
+      REP(j, n)
+        if (a[i][j]) {
+          if (! i || ! a[i-1][j]) s++;
+          if (i == m-1 || ! a[i+1][j]) s++;
+          if (! j || ! a[i][j-1]) s++;
+          if (j == n-1 || ! a[i][j+1]) s++;
+        }
+    return s;
+  }
+};
