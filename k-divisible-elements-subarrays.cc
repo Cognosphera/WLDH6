ad59c7a53a4aaa20fd5552886a18a2675569f105
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-07-28 14:07:09 +0000

    Makefile tweak

--- k-divisible-elements-subarrays.cc
+++ k-divisible-elements-subarrays.cc
@@ -2,4 +2,25 @@
 #define ALL(x) (x).begin(), (x).end()
 #define FOR(i, a, b) for (long i = (a); i < (b); i++)
 #define REP(i, n) for (long i = 0; i < (n); i++)
-using
+using pii = pair<int, int>;
+
+class Solution {
+public:
+  int countDistinct(vector<int>& a, int k, int p) {
+    int n = a.size(), ret = 0;
+    vector<char> div(n);
+    vector<pii> b;
+    REP(i, n)
+      div[i] = a[i]%p == 0;
+    REP(i, n) {
+      int c = 0;
+      FOR(j, i, n) {
+        c += div[j];
+        if (c > k) break;
+        b.emplace_back(i, j-i+1);
+      }
+    }
+    sort(ALL(b), [&](pii x, pii y) {
+      if (x.second != y.second)
+        return x.second < y.second;
+      return lexicographical
