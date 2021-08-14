bed20146bef1ebc04293dedb99033ae3333665ef
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-08-14 15:32:54 +0000

    bump 0.2.1

--- largest-color-value-in-a-directed-graph.cc
+++ largest-color-value-in-a-directed-graph.cc
@@ -6,4 +6,31 @@ class Solution {
 public:
   int largestPathValue(string colors, vector<vector<int>>& edges) {
     int n = colors.size(), top = -1, cnt = 0;
-    vec
+    vector<vector<int>> es(n);
+    int ans = 0;
+    for (auto &x: edges)
+      es[x[0]].push_back(x[1]);
+
+    REP(cc, 26) {
+      vector<int> d(n), c(n);
+      for (auto &x: edges)
+        d[x[1]]++;
+      top = -1;
+      REP(i, n) {
+        if (!d[i]) {
+          d[i] = top;
+          top = i;
+        }
+      }
+      while (~top) {
+        int u = top;
+        top = d[top];
+        cnt++;
+        c[u] += colors[u] == 'a'+cc;
+        for (int v: es[u]) {
+          c[v] = max(c[v], c[u]);
+          if (!--d[v]) {
+            d[v] = top;
+            top = v;
+          }
+        }
