ab5e6f3e37c4836052933df002a05ee8c20a3260
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-08-17 07:33:23 +0000

    assorted fixes

--- largest-component-size-by-common-factor.rs
+++ largest-component-size-by-common-factor.rs
@@ -9,4 +9,19 @@ impl Solution {
             x as usize
         }
         let m = nums.iter().max().unwrap() + 1;
- 
+        let mut sieve = (0..m).collect::<Vec<_>>();
+        for i in 2..=(m as f64).sqrt() as i32 {
+            if sieve[i as usize] != i { continue; }
+            let mut j = i*i;
+            while j < m {
+                sieve[j as usize] = i;
+                j += i;
+            }
+        }
+        let mut uf = (0..m).collect::<Vec<_>>();
+        for &x in nums.iter() {
+            let rx = find(&mut uf, x);
+            let mut x = x;
+            while x > 1 {
+                let p = sieve[x as usize];
+                let rp = find(&mut uf,
