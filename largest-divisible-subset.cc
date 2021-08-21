0065f1dd3575bb2cd44fee69db8f0bafc5ece9aa
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-08-21 07:26:14 +0000

    Updated the CHANGELOG

--- largest-divisible-subset.cc
+++ largest-divisible-subset.cc
@@ -6,4 +6,27 @@ class Solution {
 public:
   vector<int> largestDivisibleSubset(vector<int>& nums) {
     vector<pair<int, int>> dp(nums.size());
-    s
+    sort(nums.begin(), nums.end());
+    int x = -1, s = 0;
+    REP(i, nums.size()) {
+      dp[i] = {1, -1};
+      REP(j, i)
+        if (nums[i] % nums[j] == 0 && dp[j].first + 1 > dp[i].first)
+          dp[i] = {dp[j].first + 1, j};
+      if (dp[i].first > s)
+        s = dp[i].first, x = i;
+    }
+    vector<int> r;
+    for (; x >= 0; x = dp[x].second)
+      r.push_back(nums[x]);
+    return r;
+  }
+};
+
+///
+
+class Solution {
+public:
+  vector<int> largestDivisibleSubset(vector<int>& nums) {
+    unordered_map<int, pair<int, int>> dp;
+    sort(nums.beg
