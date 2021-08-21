25608b74e183168aae1c3f988ace2b437afe08ea
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-08-21 14:17:10 +0000

    add cors

--- largest-divisible-subset.cc
+++ largest-divisible-subset.cc
@@ -29,4 +29,22 @@ class Solution {
 public:
   vector<int> largestDivisibleSubset(vector<int>& nums) {
     unordered_map<int, pair<int, int>> dp;
-    sort(nums.beg
+    sort(nums.begin(), nums.end());
+    for (int x: nums) {
+      dp[x] = {1, 0};
+      for (int i = 1; i <= x/i; i++)
+        if (x%i == 0) {
+          if (dp.count(i) && dp[i].first + 1 > dp[x].first && i < x)
+            dp[x] = {dp[i].first + 1, i};
+          if (dp.count(x/i) && dp[x/i].first + 1 > dp[x].first && i > 1)
+            dp[x] = {dp[x/i].first + 1, x/i};
+        }
+    }
+    int x = 0, s = 0;
+    for (auto& it: dp)
+      if (it.second.first > s)
+        s = it.second.first, x = it.first;
+    vector<int> r;
+    for (; dp.count(x); x = dp[x].second)
+      r.push_back(x);
+    return r;
