86daaac1bf595c1b069a26fe8199961f699a3fb7
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-09-05 07:24:14 +0000

    powerpc/rtas: Validate rtas.entry before calling enter_rtas()

--- longest-common-subpath.rs
+++ longest-common-subpath.rs
@@ -5,4 +5,18 @@ impl Solution {
         let mut a = vec![];
         let mut pos = vec![];
         for i in 0..np {
-            a.append(&mut paths[i]
+            a.append(&mut paths[i]);
+            a.push(ab + i as i32);
+            pos.resize(a.len(), i as i32);
+        }
+
+        let n = a.len();
+        let m = ab as usize + np;
+        let mut sa: Vec<i32> = Vec::with_capacity(n);
+        let mut r: Vec<i32> = Vec::with_capacity(n);
+        let mut h = vec![0; n.max(m)];
+        let mut x: Vec<i32> = Vec::with_capacity(n);
+        unsafe { sa.set_len(n); r.set_len(n); x.set_len(n); }
+        for i in 0..n { h[a[i] as usize] += 1; }
+        for i in 1..m { h[i] += h[i-1]; }
+        for i in (0..n)
