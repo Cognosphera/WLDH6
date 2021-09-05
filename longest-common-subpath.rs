753ed8bea7fa240af6dbcc1092957fb06f3c61bf
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-09-05 19:25:54 +0000

    update sources

--- longest-common-subpath.rs
+++ longest-common-subpath.rs
@@ -19,4 +19,12 @@ impl Solution {
         unsafe { sa.set_len(n); r.set_len(n); x.set_len(n); }
         for i in 0..n { h[a[i] as usize] += 1; }
         for i in 1..m { h[i] += h[i-1]; }
-        for i in (0..n)
+        for i in (0..n).rev() { let v = &mut h[a[i] as usize]; *v -= 1; sa[*v as usize] = i as i32; }
+        r[sa[0] as usize] = 0;
+        for i in 1..n { r[sa[i] as usize] = r[sa[i-1] as usize] + if a[sa[i] as usize]!=a[sa[i-1] as usize] {1} else {0}; }
+        let mut k = 1;
+        while r[sa[n-1] as usize] != (n-1) as i32 {
+            for i in 0..n { h[r[sa[i] as usize] as usize] = i as i32;}
+            for i in (0..n).rev() { if sa[i] >= k as i32 {
+                let v = &mut h[r[sa[i] as usize-k] as usize]; x[*v as usize] = sa[i]-k as i32; *v -= 1; }}
+          
