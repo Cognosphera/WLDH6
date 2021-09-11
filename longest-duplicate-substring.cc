79770b50ae2f43fda786d10e53ea2edeeb05fea8
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-09-11 12:56:34 +0000

    Улучшено описание репозитория

---
+++ longest-duplicate-substring.cc
@@ -0,0 +1,8 @@
+// Longest Duplicate Substring
+#define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define ROF(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (b); --i >= (a); )
+
+class Solution {
+public:
+  string
