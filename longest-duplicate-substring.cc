0f5f5c54e90f015610daf9e375efc4f5f5a4b301
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-09-11 15:23:33 +0000

    Fixing Makefile

--- longest-duplicate-substring.cc
+++ longest-duplicate-substring.cc
@@ -5,4 +5,20 @@
 
 class Solution {
 public:
-  string
+  string longestDupSubstring(string S) {
+    const char *a = S.c_str();
+    int n = S.size(), m = 256;
+    int *sa = new int[n], *r = new int[n], *h = new int[max(n, 256)], *x = new int[n];
+    fill_n(h, m, 0);
+    copy_n(a, n, r);
+    REP(i, n) h[r[i]]++;
+    FOR(i, 1, m) h[i] += h[i-1];
+    ROF(i, 0, n) sa[--h[r[i]]] = i;
+    int k = 1;
+    for (; ; k <<= 1) {
+      iota(x, x+k, n-k);
+      int j = k;
+      REP(i, n) if (sa[i] >= k) x[j++] = sa[i]-k;
+      fill_n(h, m, 0);
+      REP(i, n) h[r[x[i]]]++;
+      FOR(i, 1, m) h[i] += h
