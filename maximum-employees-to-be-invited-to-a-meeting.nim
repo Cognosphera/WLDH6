f0001fb3a464e9f42ce7b26f9729262bc96af7f1
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-11-07 17:30:58 +0000

    Added integration tests.

--- maximum-employees-to-be-invited-to-a-meeting.nim
+++ maximum-employees-to-be-invited-to-a-meeting.nim
@@ -1,2 +1,29 @@
 # Maximum Employees to Be Invited to a Meeting
-import std/seq
+import std/sequtils
+type cintA = ptr UncheckedArray[cint]
+
+proc maximumInvitations(fa: cintA, n: int): int {.exportc.} =
+  var
+    d = newSeq[int](n)
+    dp = newSeqWith[int](n, 1)
+    vis = newSeq[bool](n)
+  var top = -1
+  for i in 0..<n: d[fa[i]] += 1
+  for i in 0..<n:
+    if d[i] == 0:
+      d[i] = top
+      top = i
+      vis[i] = true
+  while top >= 0:
+    let u = top
+    top = d[top]
+    let v = fa[u]
+    dp[v] = max(dp[v], dp[u]+1)
+    d[v] -= 1
+    if d[v] == 0:
+      d[v] = top
+      top = v
+      vis[v] = true
+  var sum = 0
+  for i in 0..<n:
+    if vis[i]
