1541bd1f8a9ea9ba0e73daca43067b0648fb330f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-11-13 08:44:04 +0000

    trailing spaces

--- maximum-number-of-non-overlapping-palindrome-substrings.cc
+++ maximum-number-of-non-overlapping-palindrome-substrings.cc
@@ -26,4 +26,24 @@ public:
 class Solution {
 public:
   int maxPalindromes(string s, int k) {
-    int n = s.size(), m = 2*n+1
+    int n = s.size(), m = 2*n+1;
+    string a(m, '.');
+    for (int i = 0; i < n; i++)
+      a[2*i+1] = s[i];
+    vector r(m, 0), f(n+1, 0);
+    for (int f, g = 0, i = 0; i < m; i++)
+      if (i < g && r[2*f-i] != g-i)
+        r[i] = min(r[2*f-i], g-i);
+      else {
+        f = i;
+        g = max(g, i);
+        for (; g < m && 2*f >= g && a[2*f-g] == a[g]; g++);
+        r[i] = g-f;
+      }
+    for (int i = k; i <= n; i++)
+      f[i] = max({f[i-1], r[2*i-k] >= k+1 ? f[i-k]+1 : 0, r[2*i-k-1] >= k+2 ? f[i-k-1]+1 : 0});
+    return f[n];
+  }
+};
+
+/// Manacher's algorithm (wit
