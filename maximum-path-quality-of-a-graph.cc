f07e07e54bb90b95ff72993592c109f7bc45c977
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-11-17 22:30:14 +0000

    pg

--- maximum-path-quality-of-a-graph.cc
+++ maximum-path-quality-of-a-graph.cc
@@ -10,4 +10,23 @@ public:
     if (!cnt[u]++)
       val += values[u];
     if (u == 0)
-      ans = max(ans, v
+      ans = max(ans, val);
+    for (auto [v, w]: es[u])
+      if (tim >= w)
+        dfs(tim-w, v, val);
+    if (!--cnt[u])
+      val -= values[u];
+  }
+  int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
+    int n = values.size();
+    this->values = move(values);
+    es.resize(n);
+    cnt.resize(n);
+    for (auto &e: edges) {
+      es[e[0]].emplace_back(e[1], e[2]);
+      es[e[1]].emplace_back(e[0], e[2]);
+    }
+    dfs(maxTime, 0, 0);
+    return ans;
+  }
+};
---
+++ maximum-performance-of-a-team.rs
@@ -0,0 +1,3 @@
+// Maximum Performance of a Team
+impl Solution {
+    pub fn max_performance(n: 
