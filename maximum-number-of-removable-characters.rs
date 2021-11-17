0239d349702df44b43833878c184ca766982f55f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-11-17 05:49:53 +0000

    1.0.0 released

--- maximum-number-of-removable-characters.rs
+++ maximum-number-of-removable-characters.rs
@@ -3,4 +3,21 @@ impl Solution {
     pub fn maximum_removals(s: String, p: String, removable: Vec<i32>) -> i32 {
         let s = s.as_bytes();
         let p = p.as_bytes();
-        let mut l = 
+        let mut l = 0;
+        let mut h = removable.len();
+        let mut del = vec![false; s.len()];
+        while l < h {
+            let m = l+h+1 >> 1;
+            for i in 0..m { del[removable[i] as usize] = true; }
+            let mut j = 0;
+            for i in 0..s.len() {
+                if !del[i] && s[i] == p[j] {
+                    j += 1;
+                    if j == p.len() { break; }
+                }
+            }
+            for i in 0..m { del[removable[i] as usize] = false; }
+            if j == p.len() {
+                l = m;
+            } else {
+   
