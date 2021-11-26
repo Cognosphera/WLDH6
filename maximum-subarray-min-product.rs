67492ee65f5c0ada03e8912d0f03a8743564eb02
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-11-26 14:33:53 +0000

    Release v0.0.4

--- maximum-subarray-min-product.rs
+++ maximum-subarray-min-product.rs
@@ -10,4 +10,21 @@ impl Solution {
                 st.pop();
             }
             if let Some(&j) = st.last() { l[i] = j+1; }
-     
+            st.push(i);
+        }
+        let sum: Box<[i64]> = std::iter::once(0).chain(a.iter().scan(0, |s,&y| { *s += y as i64; Some(*s) })).collect();
+        let mut ans = 0;
+        for i in 0..a.len() {
+            ans = ans.max((sum[r[i]]-sum[l[i]]) * a[i] as i64);
+        }
+        (ans % 1000000007) as i32
+    }
+}
+
+///
+
+impl Solution {
+    pub fn max_sum_min_product(a: Vec<i32>) -> i32 {
+        use std::{iter,usize::MAX};
+        let sum: Box<[i64]> = iter::once(0).chain(a.iter().scan(0, |s,&y| { *s += y as i64; Some(*s) })).collect();
+        
