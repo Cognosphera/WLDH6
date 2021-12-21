54db86dbb66e1e0dd34e6419286fa7cc5ac723f5
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-12-21 05:41:59 +0000

    Preparing release 0.4

--- minimum-adjacent-swaps-for-k-consecutive-ones.nim
+++ minimum-adjacent-swaps-for-k-consecutive-ones.nim
@@ -6,3 +6,14 @@ proc minMoves(nums: ptr UncheckedArray[cint], n: int, k: int): int {.exportc.} =
     s = 0
   for i in 0..<n:
     if nums[i] > 0:
+      a.add s
+      s += i-a.len
+  a.add s
+  result = int.high
+  for i in 0..<a.len-k:
+    let
+      m = i+k div 2
+      x = a[m+1]-a[m]
+      left = x*(m-i) - (a[m]-a[i])
+      right = a[i+k]-a[m] - x*(i+k-m)
+    result = min(result, left+right)
---
+++ minimum-adjacent-swaps-to-reach-the-kth-smallest-number.rs
@@ -0,0 +1,25 @@
+
+impl Solution {
+    fn next_permutation(a: &mut [u8]) {
+        for i in (0..a.len()-1).rev() {
+            if a[i] >= a[i+1] { continue; }
+            let mut j = a.len()-1;
+            while a[i] >= a[j] { j -= 1; }
+            a.swap(i, j);
+            a[i+1..].reverse();
+            break;
+        }
+    }
+    pub fn get_min_swaps(num: String, k: i32) -> i32 {
+        let mut num: Vec<u8> = num.as_bytes().to_vec();
+        let mut num1 = num.clone();
+        for _ in 0..k {
+            Self::next_permutation(&mut num1);
+        }
+        num1.into_iter().map(|x| {
+            let j = num.iter().position(|&y| x == y).unwrap();
+            num.remove(j);
+            j as i32
+        }).sum()
+    }
+}
