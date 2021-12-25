6faa2da7c8129e814ca89dc489b6bb44eabb03c3
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-12-25 06:37:51 +0000

    Fixes a typo

--- minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cc
+++ minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cc
@@ -5,4 +5,20 @@ class Solution {
   static constexpr int dir[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 public:
   int minCost(vector<vector<int>> &g) {
-    int m
+    int m = g.size(), n = g[0].size(), ret = 0, fore = 0;
+    vector<vector<int>> d(m, vector<int>(n, -1));
+    vector<pair<int, int>> q;
+    auto flood = [&](unsigned x, unsigned y) {
+      while (x < m && y < n && d[x][y] == -1) {
+        d[x][y] = ret;
+        q.emplace_back(x, y);
+        const int *next = dir[g[x][y]-1];
+        x += next[0];
+        y += next[1];
+      }
+    };
+    flood(0, 0);
+    while (d[m-1][n-1] == -1) {
+      ret++;
+      for (int x, y, rear = q.size(); fore != rear; ) {
+        tie(x, y) = q
