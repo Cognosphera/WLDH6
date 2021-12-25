b5cec75be7c47412a8bece8c8f15d2634b0088b4
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2021-12-25 18:12:12 +0000

    Tweak might work

--- minimum-cost-to-merge-stones.cc
+++ minimum-cost-to-merge-stones.cc
@@ -28,4 +28,21 @@ public:
 
 #define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
 #define REP(i, n) FOR(i, 0, n)
-#def
+#define ROF(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (b); --i >= (a); )
+
+class Solution {
+public:
+  int mergeStones(vector<int>& a, int K) {
+    int n = a.size();
+    if ((n-1)%--K) return -1;
+    vector<int> sum(n+1);
+    REP(i, n)
+      sum[i+1] = sum[i] + a[i];
+    vector<vector<int>> s(n, vector<int>(n)), u(n, vector<int>(n));
+    ROF(i, 0, n)
+      FOR(j, i, n)
+        if (j-i < K)
+          u[i][j] = i;
+        else {
+          int t = INT_MAX, tt, l = u[i][j-K], h = min(u[i+K][j], j-1);
+          for (int g = l; g <= h;
