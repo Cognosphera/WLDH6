3e727f5d224eda93a6d0c619ef90f8fbfb5d91ca
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-01-02 18:46:59 +0000

    added new images

--- minimum-cost-to-reach-destination-in-time.rs
+++ minimum-cost-to-reach-destination-in-time.rs
@@ -6,4 +6,16 @@ impl Solution {
         let n = passing_fees.len();
         let mut g = vec![vec![]; n];
         for e in edges.into_iter() {
-            g[e[0] as usize]
+            g[e[0] as usize].push((e[1], e[2]));
+            g[e[1] as usize].push((e[0], e[2]));
+        }
+        let mut dist = vec![max_time+1; n];
+        let mut h: BinaryHeap<Reverse<(i32, i32, i32)>> = BinaryHeap::from(vec![Reverse((passing_fees[0], 0, 0))]);
+        while let Some(Reverse((fee, c, t))) = h.pop() {
+            let c = c as usize;
+            if dist[c] <= t { continue; }
+            dist[c] = t;
+            if c == n-1 { return fee; }
+            for &e in g[c].iter() {
+                if t+e.1 < dist[e.0 as usize] {
+                    h.push(Reverse((f
