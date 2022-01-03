b3b7341cc3695b986f63f339cd25a91733bb3072
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-01-03 13:36:25 +0000

    Personal Website

--- minimum-difference-in-sums-after-removal-of-elements.cc
+++ minimum-difference-in-sums-after-removal-of-elements.cc
@@ -15,4 +15,30 @@ public:
     FOR(i, n, 2*n) {
       if (a[i] < heap[0]) {
         sum = sum-heap[0]+a[i];
-        pop_heap(ALL(hea
+        pop_heap(ALL(heap));
+        heap[n-1] = a[i];
+        push_heap(ALL(heap));
+      }
+      l[i-n+1] = sum;
+    }
+
+    heap.assign(a.end()-n, a.end());
+    make_heap(ALL(heap), greater<>());
+    sum = accumulate(ALL(heap), 0L);
+    r[n] = sum;
+    ROF(i, n, 2*n) {
+      if (a[i] > heap[0]) {
+        sum = sum-heap[0]+a[i];
+        pop_heap(ALL(heap), greater<>());
+        heap[n-1] = a[i];
+        push_heap(ALL(heap), greater<>());
+      }
+      r[i-n] = sum;
+    }
+
+    long ret = l[0]-r[0];
+    FOR(i, 1, n+1)
+      ret = min(ret, l[i]-r[i]);
+    return ret;
+  }
+};
