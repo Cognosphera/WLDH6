e52e2d27e6ddf3dc10bc4fae041c556f98017e29
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-01-06 15:47:26 +0000

    bump version 1.1.0

--- minimum-interval-to-include-each-query.rs
+++ minimum-interval-to-include-each-query.rs
@@ -1,1 +1,14 @@
-// Minimum Interval to Include Each 
+// Minimum Interval to Include Each Query
+/// scan line algorithm + heap
+impl Solution {
+    pub fn min_interval(mut intervals: Vec<Vec<i32>>, queries: Vec<i32>) -> Vec<i32> {
+        use std::cmp::Reverse;
+        use std::collections::BinaryHeap;
+        intervals.sort_unstable_by_key(|itv| itv[0]);
+        let mut i = 0;
+        let mut h: BinaryHeap<Reverse<(i32, i32)>> = BinaryHeap::new();
+        let mut queries: Vec<(usize, i32)> = queries.into_iter().enumerate().collect();
+        queries.sort_unstable_by_key(|x| x.1);
+        let mut ans = vec![-1; queries.len()];
+        for (qi, q) in queries {
+      
