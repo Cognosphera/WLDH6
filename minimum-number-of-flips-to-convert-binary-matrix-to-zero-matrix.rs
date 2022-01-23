f8278cf55cf9765184915a6754f75476e3ad55e7
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-01-23 13:41:16 +0000

    merging develop

--- minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.rs
+++ minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.rs
@@ -14,4 +14,26 @@ impl Solution {
         for k0 in 0..1i32<<(m*n) {
             let mut k = 0;
             for i in 0..m { k |= (k0>>i*n & row) << i*(n+1); }
-            if ((k ^ k>>1 ^ k<<1 ^ k>>n+1 ^ k<<n+1) & full) == b
+            if ((k ^ k>>1 ^ k<<1 ^ k>>n+1 ^ k<<n+1) & full) == bgn {
+                ans = ans.min(k.count_ones());
+            }
+        }
+        if ans == std::u32::MAX {-1} else {ans as i32}
+    }
+}
+
+///
+
+impl Solution {
+    pub fn min_flips(mat: Vec<Vec<i32>>) -> i32 {
+        let m = mat.len();
+        let n = mat[0].len();
+        let row = (1<<n)-1;
+        let mut bgn = 0;
+        let mut full = 0;
+        for i in 0..m {
+            for j in 0..n {
+                if mat[i][j] != 0 { bgn |= 1 << i*(n+1)+j; }
+                full |= 1 << i*(n+1)+j; }}
+        for ans in 0..=m*n {
+            let mut k0 = (1i32
