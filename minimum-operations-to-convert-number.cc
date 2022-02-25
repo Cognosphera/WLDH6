c6d790a479c7d64e56a65596b9089784ebe92d4d
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-02-25 10:17:46 +0000

    update REAMDE

--- minimum-operations-to-convert-number.cc
+++ minimum-operations-to-convert-number.cc
@@ -3,4 +3,21 @@
 
 class Solution {
 public:
-  int m
+  int minimumOperations(vector<int>& a, int start, int goal) {
+    const int N = 1001;
+    vector<int> d(N, -1), q{start};
+    d[start] = 0;
+    REP(i, q.size()) {
+      int u = q[i];
+      REP(j, a.size())
+        for (int x : {u+a[j], u-a[j], u^a[j]}) {
+          if (x == goal) return d[u]+1;
+          if (unsigned(x) < N && d[x] < 0) {
+            d[x] = d[u]+1;
+            q.push_back(x);
+          }
+        }
+    }
+    return -1;
+  }
+};
---
+++ minimum-operations-to-make-a-uni-value-grid.nim
@@ -0,0 +1,4 @@
+# Minimum Operations to Make a Uni-Value Grid
+import std/algorithm
+
+proc minOperations(grid: ptr Unchecke
