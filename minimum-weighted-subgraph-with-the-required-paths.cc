329bf341abd8965e2667fff01c2be6232c7d1f92
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-03-02 18:03:31 +0000

    Updated unit test

--- minimum-weighted-subgraph-with-the-required-paths.cc
+++ minimum-weighted-subgraph-with-the-required-paths.cc
@@ -20,3 +20,50 @@ void heap_down(int n, int x) {
   }
   lkp[heap[x] = key] = x;
 }
+
+class Solution {
+  using pii = pair<int, int>;
+public:
+  long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
+    auto dijkstra = [&](const vector<pii> *e, int src) -> vector<long> {
+      int size = 1;
+      fill_n(dist, n, LONG_MAX);
+      dist[src] = 0;
+      fill_n(lkp, n, -1);
+      lkp[heap[0] = src] = 0;
+      while (size) {
+        int u = exchange(heap[0], heap[--size]);
+        lkp[u] = -1;
+        if (size) heap_down(size, 0);
+        if (dist[u] == LONG_MAX) break;
+        for (auto &it: e[u]) {
+          long w = dist[u]+it.second;
+          int v = it.first;
+          if (w < dist[v]) {
+            dist[v] = w;
+            if (lkp[v] == -1) {
+              lkp[heap[size] = v] = size;
+              size++;
+            }
+            heap_up(size, lkp[v]);
+          }
+        }
+      }
+      return vector<long>(dist, dist+n);
+    };
+    auto e = make_unique<vector<pii>[]>(n);
+    auto r = make_unique<vector<pii>[]>(n);
+    for (auto &x: edges) {
+      e[x[0]].emplace_back(x[1], x[2]);
+      r[x[1]].emplace_back(x[0], x[2]);
+    }
+    auto d1 = dijkstra(e.get(), src1);
+    auto d2 = dijkstra(e.get(), src2);
+    auto d3 = dijkstra(r.get(), dest);
+    long ret = LONG_MAX;
+    for (int i = 0; i < n; i++)
+      if (d1[i] != LONG_MAX && d2[i] != LONG_MAX && d3[i] != LONG_MAX)
+        ret = min(ret, d1[i]+d2[i]+d3[i]);
+    return ret == LONG_MAX ? -1 : ret;
+  }
+};
---
+++ missing-ranges.cc
@@ -0,0 +1,4 @@
+// Missing Ranges
+class Solution {
+  string f(int l, int u) {
+    return l == u ? to_string(l) : to_st
