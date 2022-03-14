820411dc525914208a16a0e7d3fe510fcac852b2
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-03-14 22:40:42 +0000

    tick version

--- number-of-different-subsequences-gcds.cc
+++ number-of-different-subsequences-gcds.cc
@@ -3,4 +3,16 @@ class Solution {
 public:
   int countDifferentSubsequenceGCDs(vector<int>& nums) {
     int mx = *max_element(nums.begin(), nums.end()), ans = 0;
-    v
+    vector<char> in(mx+1);
+    for (int x: nums)
+      in[x] = 1;
+    for (int i = 1; i <= mx; i++) {
+      int g = 0;
+      for (int j = i; j <= mx; j += i)
+        if (in[j] && (g = gcd(g, j)) == i)
+          break;
+      ans += g == i;
+    }
+    return ans;
+  }
+};
