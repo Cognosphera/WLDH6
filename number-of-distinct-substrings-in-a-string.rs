fc5fe30b95218433aa5db224e101c2bde008970b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-03-15 15:08:01 +0000

    Rotation

--- number-of-distinct-substrings-in-a-string.rs
+++ number-of-distinct-substrings-in-a-string.rs
@@ -1,1 +1,13 @@
 impl Solution {
+    pub fn count_distinct(a: String) -> i32 {
+        let a = a.as_bytes();
+        let n = a.len();
+        let m = 127;
+        let mut sa: Vec<i32> = Vec::with_capacity(n);
+        let mut r: Vec<i32> = Vec::with_capacity(n);
+        let mut h = vec![0; n.max(m)];
+        let mut x: Vec<i32> = Vec::with_capacity(n);
+        unsafe { sa.set_len(n); r.set_len(n); x.set_len(n); }
+        for i in 0..n { h[a[i] as usize] += 1; }
+        for i in 1..m { h[i] += h[i-1]; }
+        for i in (0..n).rev() { let v = &mut h[a[i] as usize]; *v -= 1; sa[*v as usiz
