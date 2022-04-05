646b9bed930ade9d6129f77efcb1ff0ab9d10ebd
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-04-05 21:53:44 +0000

    Adding Readme

--- number-of-squareful-arrays.cc
+++ number-of-squareful-arrays.cc
@@ -11,4 +11,26 @@ public:
       int q = sqrt(A[0]);
       return q*q == A[0];
     }
-    vector<vector<char>> g(n, vect
+    vector<vector<char>> g(n, vector<char>(n));
+    REP(i, n)
+      REP(j, n) {
+        int t = A[i]+A[j], q = sqrt(t);
+        g[i][j] = q*q == t;
+      }
+    vector<vector<int>> p(n, vector<int>(nn)), q(n, vector<int>(nn));
+    REP(i, n)
+      p[i][1<<i] = 1;
+    REP(i, n-1) {
+      REP(j, n)
+        fill(ALL(q[j]), 0);
+      REP(j, n)
+        REP(k, nn)
+          if (p[j][k])
+            REP(jj, n)
+              if (!(k & 1<<jj) && g[j][jj])
+                q[jj][k | 1<<jj] += p[j][k];
+      p.swap(q);
+    }
+    int ans = 0;
+    REP(j, n)
+      ans += accumulate(ALL(
