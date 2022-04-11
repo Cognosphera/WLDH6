4acfad1d920789a532bd538206f3e6e2c7f9480a
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-04-11 13:01:01 +0000

    Add explicit keywords.

---
+++ number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cc
@@ -0,0 +1,6 @@
+#define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define ROF(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (b); --i >= (a); )
+typedef complex<double> cd;
+
+const long MOD = 1000000007, 
