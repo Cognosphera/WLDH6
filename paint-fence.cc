533cf991ba883a219acb169969aee890306c6dfb
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-04-20 19:31:28 +0000

    added benchmarks

--- paint-fence.cc
+++ paint-fence.cc
@@ -2,4 +2,12 @@
 class Solution {
 public:
   int numWays(int n, int k) {
-    int a = 0, b = k, c
+    int a = 0, b = k, c = k*k;
+    while (n--) {
+      a = b;
+      b = c;
+      c = (a+b)*(k-1);
+    }
+    return a;
+  }
+};
