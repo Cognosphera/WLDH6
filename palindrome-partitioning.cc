2a648b9ec0a29bed7f7117a58f280abf39d83108
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-05-04 08:37:22 +0000

    update meta tags

--- palindrome-partitioning.cc
+++ palindrome-partitioning.cc
@@ -1,3 +1,27 @@
 // Palindrome Partitioning
 #define ROF(i, a, b) for (int i = (b); --i >= (a); )
-#define FOR(i, a, b
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+class Solution {
+private:
+  vector<vector<bool>> f;
+  vector<string> rr;
+  vector<vector<string>> r;
+  void g(string &s, int i) {
+    if (i == s.size())
+      r.push_back(rr);
+    else
+      REP(j, s.size())
+        if (f[i][j]) {
+          rr.push_back(s.substr(i, j-i+1));
+          g(s, j+1);
+          rr.pop_back();
+        }
+  }
+public:
+  vector<vector<string>> partition(string s) {
+    int n = s.size();
+    f.assign(n, vector<bool>(n));
+    ROF(i, 0, n) {
+      f[i][i] = t
