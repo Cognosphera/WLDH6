89e89fb66977ed0b232e506a821ff3869a459de5
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-05-23 11:18:06 +0000

    fixed notice

--- peeking-iterator.cc
+++ peeking-iterator.cc
@@ -3,4 +3,26 @@
 // Below is the interface for Iterator, which is already defined for you.
 // **DO NOT** modify the interface for Iterator.
 class Iterator {
-  s
+  struct Data;
+  Data* data;
+public:
+  Iterator(const vector<int>& nums);
+  Iterator(const Iterator& iter);
+  virtual ~Iterator();
+  // Returns the next element in the iteration.
+  int next();
+  // Returns true if the iteration has more elements.
+  bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+  bool f;
+  int x;
+public:
+  PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+    // Initialize any member here.
+    // **DO NOT** save a copy of nums and manipulate it directly.
+    // You should only use the Iterator interface methods.
+    f = false;
+  }
