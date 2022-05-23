5184fbc5c4eb4e8b87314b0ae2fdead398458228
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-05-23 20:36:29 +0000

    Update bazarloader.txt

--- people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.rs
+++ people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.rs
@@ -1,3 +1,18 @@
 // People Whose List of Favorite Companies Is Not a Subset of Another List
 impl Solution {
-    pub fn people_indexes(favorite_compani
+    pub fn people_indexes(favorite_companies: Vec<Vec<String>>) -> Vec<i32> {
+        use std::collections::HashMap;
+        let n = favorite_companies.len();
+        let mut a = vec![vec![]; n];
+        let mut c2i: HashMap<String, usize> = HashMap::new();
+        for (i,cs) in favorite_companies.iter().enumerate() {
+            for c in cs.iter() {
+                let len = c2i.len();
+                let j = *c2i.entry(c.clone()).or_insert(len);
+                a[i].push(j);
+            }
+        }
+        for i in 0..n { a[i].sort_unstable(); }
+        let mut ans = vec![];
+        for i in 0..n {
+            let mut ok
