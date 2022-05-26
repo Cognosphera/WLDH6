d7d94568b44577242379faa00305dd8e4e1a8684
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-05-26 05:26:53 +0000

    inti

--- perfect-rectangle.cc
+++ perfect-rectangle.cc
@@ -21,4 +21,26 @@ public:
       int& m1 = m[make_pair(a[0], a[1])]; if (m1 & 1) return false; m1 |= 1;
       int& m2 = m[make_pair(a[0], a[3])]; if (m2 & 2) return false; m2 |= 2;
       int& m4 = m[make_pair(a[2], a[3])]; if (m4 & 4) return false; m4 |= 4;
-      int& m8 = m[ma
+      int& m8 = m[make_pair(a[2], a[1])]; if (m8 & 8) return false; m8 |= 8;
+    }
+    for (auto& i: m) {
+      int x, y, mask = i.second;
+      tie(x, y) = i.first;
+      if ((x == x0 || x == x1) && (y == y0 || y == y1)) {
+        if (__builtin_popcount(mask) != 1)
+          return false;
+      } else {
+        if (mask != 3 && mask != 6 && mask != 12 && mask != 9 && mask != 15)
+          return false;
+      }
+    }
+    return true;
+  }
+};
+
+/// sweep line
+
+class Solution {
+public:
+  bool isRectangleCover(vector<vector<int>>& rectangles) {
+    long area = 0, x0 = INT_MAX, x1 = INT_MIN, y0 = INT_MAX, y1 = INT_MIN
