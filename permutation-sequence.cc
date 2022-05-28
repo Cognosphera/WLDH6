a795d061eb5e600c6ecd0d9df4026430bc4e1260
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-05-28 17:28:20 +0000

    update routes

--- permutation-sequence.cc
+++ permutation-sequence.cc
@@ -4,4 +4,35 @@
 class Solution {
 public:
   string getPermutation(int n, int k) {
-    int f[] = {1,1,2,6,24,
+    int f[] = {1,1,2,6,24,120,720,5040,40320};
+    vector<bool> a(n, true);
+    string r;
+    k--;
+    ROF(i, 0, n) {
+      int t = k/f[i], j = 0;
+      k %= f[i];
+      while (! a[j]) j++;
+      while (t--)
+        while (! a[++j]);
+      a[j] = false;
+      r += '1'+j;
+    }
+    return r;
+  }
+};
+
+// bit twiddling
+
+#define ROF(i, a, b) for (int i = (b); --i >= (a); )
+
+class Solution {
+public:
+  string getPermutation(int n, int k) {
+    int f[] = {1,1,2,6,24,120,720,5040,40320};
+    int a = ~0;
+    string r;
+    k--;
+    ROF(i, 0, n) {
+      int t = k/f[i];
+      k %= f[i];
+   
