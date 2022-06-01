be785a95028861e518d1c0f80217c2a751dd3741
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-06-01 09:08:45 +0000

    Fix copyright notice.

--- populating-next-right-pointers-in-each-node-ii.cc
+++ populating-next-right-pointers-in-each-node-ii.cc
@@ -9,4 +9,23 @@ public:
         r = NULL;
         if (p->left) {
           if (p->right)
-  
+            p->left->next = p->right;
+          r = p->left;
+        } else if (p->right)
+          r = p->right;
+        if (r) {
+          if (q)
+            q->next = r;
+          else
+            pp = q = r;
+          if (q->next)
+            q = q->next;
+          if (q->next)
+            q = q->next;
+        }
+      }
+      p = pp;
+      pp = NULL;
+    }
+  }
+};
---
+++ populating-next-right-pointers-in-each-node.cc
@@ -0,0 +1,6 @@
+// Populating Next Right Pointers in Each Node
+class Solution {
+public:
+  void connect(TreeLinkNode *root) {
+    if (! root) return;
+    if (r
