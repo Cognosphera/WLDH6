47e7372271ea6efc096a3bf39c915cd56d6dc5e7
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-06-11 21:51:24 +0000

    Fix for trunk llvm

---
+++ put-marbles-in-bags.cc
@@ -0,0 +1,7 @@
+// Put Marbles in Bags
+#define ALL(x) (x).begin(), (x).end()
+#define REP(i, n) for (int i = 0; i < (n); i++)
+
+class Solution {
+public:
+  long long putMarbles(vector<int>& a, int 
