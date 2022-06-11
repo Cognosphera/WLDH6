5b740843574e6dcfd47febdf844a4484b571137b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-06-11 05:51:08 +0000

    Organized Imports

--- process-tasks-using-servers.rs
+++ process-tasks-using-servers.rs
@@ -12,4 +12,17 @@ impl Solution {
             free.push(Reverse((servers[i], i as i32)));
         }
         for i in 0..tasks.len() {
-            if free.is_empty() { tick = (
+            if free.is_empty() { tick = (busy.peek().unwrap().0).0; }
+            else { tick = tick.max(i as i32); }
+            while let Some(Reverse(top)) = busy.peek() {
+                if top.0 != tick { break; }
+                free.push(Reverse((servers[top.1 as usize], top.1)));
+                busy.pop();
+            }
+            let u = (free.pop().unwrap().0).1;
+            ans[i] = u;
+            busy.push(Reverse((tick+tasks[i], u)));
+        }
+        ans
+    }
+}
---
+++ projection-area-of-3d-shapes.cc
@@ -0,0 +1,3 @@
+// Projection Area of 3D Shapes
+#define ALL(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (remove_cv<remo
