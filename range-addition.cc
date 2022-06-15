2def13e0af2c881650bcf8ae49db8990e8ab6f43
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-06-15 20:38:32 +0000

    More travis

--- range-addition.cc
+++ range-addition.cc
@@ -7,4 +7,15 @@ public:
       for (; x < a.size(); x |= x+1)
         a[x] += y;
     };
-    for (
+    for (auto& u: updates) {
+      f(u[0], u[2]);
+      f(u[1]+1, - u[2]);
+    }
+    for (int i = 1; i < a.size(); i++) {
+      int j = (i & i+1) - 1;
+      if (j >= 0)
+        a[i] += a[j];
+    }
+    return a;
+  }
+};
