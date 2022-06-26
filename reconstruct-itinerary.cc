17dfc61bfec4365f72279f491c2646986f5c757c
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-06-26 12:09:10 +0000

    link change

--- reconstruct-itinerary.cc
+++ reconstruct-itinerary.cc
@@ -8,4 +8,25 @@ class Solution {
     while (! m[x].empty()) {
       string y = m[x].back();
       m[x].pop_back();
-  
+      hierholzer(y);
+    }
+    ret.push_back(x);
+  }
+public:
+  vector<string> findItinerary(vector<vector<string>> &tickets) {
+    sort(tickets.begin(), tickets.end(), greater<>());
+    for (auto &x: tickets)
+      m[x[0]].push_back(x[1]);
+    hierholzer("JFK");
+    reverse(ret.begin(), ret.end());
+    return ret;
+  }
+};
+
+/// non-recursive Hierholzer's algorithm
+
+class Solution {
+public:
+  vector<string> findItinerary(vector<vector<string>> &tickets) {
+    sort(tickets.begin(), tickets.end(), greater<>());
+    unordered_map<string, vector<string>> m;
