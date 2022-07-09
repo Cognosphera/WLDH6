b589555de352b67285b118400af3b57f7c83cfa0
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-09 10:10:44 +0000

    gcc

--- regions-cut-by-slashes.cc
+++ regions-cut-by-slashes.cc
@@ -5,4 +5,22 @@
 
 class Solution {
 public:
-  int regio
+  int regionsBySlashes(vector<string> &g) {
+    int n = g.size();
+    vector<int> uf(n*n*4);
+    iota(ALL(uf), 0);
+    auto f = [&](int x) {
+      while (uf[x] != x)
+        uf[x] = uf[uf[x]], x = uf[x];
+      return x;
+    };
+    auto me = [&](int x, int y) { uf[f(x)] = f(y); };
+    REP(i, n) REP(j, n) {
+      int t = (i*n+j)*4;
+      switch (g[i][j]) {
+      case '/': me(t+0, t+1); me(t+2, t+3); break;
+      case '\\': me(t+0, t+3); me(t+1, t+2); break;
+      default: me(t+0, t+1); me(t+1, t+2); me(t+2, t+3); break;
+      }
+      if (i) me(t+0, t-4*n+2);
+      i
