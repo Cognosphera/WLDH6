232f7a6980a5ed7d4c7e5fa0f18db217db59bebd
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-19 11:51:41 +0000

    build: Updating mediawiki/minus-x to 0.2.1

--- remove-element.cc
+++ remove-element.cc
@@ -4,4 +4,8 @@ public:
   int removeElement(vector<int> &a, int val) {
     int i = 0, j = 0;
     for (; i < a.size(); i++)
-      if (a[i] != v
+      if (a[i] != val)
+        a[j++] = a[i];
+    return j;
+  }
+};
