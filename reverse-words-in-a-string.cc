e938643082e3f991f4f0cc656716d4c1cf0ad14b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-29 18:30:17 +0000

    Increased version number

--- reverse-words-in-a-string.cc
+++ reverse-words-in-a-string.cc
@@ -5,4 +5,14 @@ public:
       size_t i, j = 0;
       for (i = 0; i < s.size(); i++)
         if (s[i] != ' ' || (j && s[j-1] != ' '))
-     
+            s[j++] = s[i];
+      while (j && s[j-1] == ' ')
+        j--;
+      s.resize(j);
+      reverse(s.begin(), s.end());
+      for (size_t i = 0, j; i < s.size(); i = j+1) {
+        for (j = i; j < s.size() && s[j] != ' '; j++);
+        reverse(s.begin()+i, s.begin()+j);
+      }
+    }
+};
