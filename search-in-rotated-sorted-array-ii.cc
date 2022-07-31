ef030d52d764884d77d4a4648666ef8c2b5a058a
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-31 22:52:55 +0000

    Build script fix

--- search-in-rotated-sorted-array-ii.cc
+++ search-in-rotated-sorted-array-ii.cc
@@ -17,4 +17,34 @@ public:
         if (a[l] <= target || a[l] >= a[m])
           h = m;
         else
-          l =
+          l = m+1;
+      } else
+        return true;
+    }
+    return false;
+  }
+};
+
+///
+
+class Solution {
+public:
+  bool search(vector<int> &a, int target) {
+    int l = 0, h = a.size();
+    while (l < h) {
+      int m = l+h >> 1;
+      if (a[m] == target) return true;
+      if (a[l] < a[m]) {
+        if (a[l] <= target && target < a[m])
+          h = m;
+        else
+          l = m+1;
+      } else if (a[l] > a[m]) {
+        if (a[m] < target && target <= a[h-1])
+          l = m+1;
+        else
+          h = m;
+      } else
+        l++;
+    }
+    return f
