bc3dbbcfe28c12609cabf574b993f5379a161492
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-31 08:25:43 +0000

    Add a description

---
+++ rotting-oranges.cc
@@ -0,0 +1,7 @@
+// Rotting Oranges
+#define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+
+class Solution {
+public:
+  int orangesRotting(vector<vector<int>>
