b85d1dcae8e2b309d980109fb699cc48c57889a1
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-07-31 09:19:54 +0000

    udldrmfb: Fix EDID not working with monitors with EDID extension blocks

--- rotting-oranges.cc
+++ rotting-oranges.cc
@@ -4,4 +4,24 @@
 
 class Solution {
 public:
-  int orangesRotting(vector<vector<int>>
+  int orangesRotting(vector<vector<int>>& g) {
+    const int dx[] = {-1, 0, 1, 0};
+    const int dy[] = {0, 1, 0, -1};
+    int m = g.size(), n = g[0].size(), x, y;
+    queue<pair<int, int>> q;
+    REP(i, m) REP(j, n)
+      if (g[i][j] == 2)
+        q.emplace(i, j);
+    while (q.size()) {
+      tie(x, y) = q.front();
+      q.pop();
+      REP(i, 4) {
+        unsigned xx = x+dx[i], yy = y+dy[i];
+        if (xx < m && yy < n && g[xx][yy] == 1)
+          g[xx][yy] = g[x][y]+1, q.emplace(xx, yy);
+      }
+    }
+    int ans = 0;
+    REP(i, m) REP(j, n) {
+      if (g[i][j] == 1)
+        return -1;
