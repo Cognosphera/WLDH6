75477aebaab3e4827a0128ef236870ec624013d5
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-07 15:15:21 +0000

    drm/radeon: fix 1 RB harvest config setup for TN/RL

---
+++ shift-2d-grid.cc
@@ -0,0 +1,7 @@
+
+// Shift 2D Grid
+#define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+
+class Solution {
+public:
