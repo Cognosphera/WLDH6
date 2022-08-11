3aab249470e915ded759fd5ea22980ee3a33fd7f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-11 14:27:25 +0000

    update video

--- shuffle-an-array.cc
+++ shuffle-an-array.cc
@@ -19,4 +19,6 @@ public:
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(nums);
- * vector<int> param_1 =
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
