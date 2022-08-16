e6469a68126f0aaee0bff1d193c2c47022a61ea9
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-16 17:20:16 +0000

    added icon

--- sliding-window-median.cc
+++ sliding-window-median.cc
@@ -24,3 +24,20 @@ class Solution {
       fenwick[x] += v;
   }
   int select(int k, int n) {
+    int r = -1;
+    for (int x = 1<<31-__builtin_clz(n); x; x >>= 1)
+      if (r+x < n && fenwick[r+x] <= k)
+        k -= fenwick[r += x];
+    return r+1;
+  }
+public:
+  vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+    vector<double> r;
+    vector<int> a = nums;
+    int n = nums.size();
+    fenwick.assign(n, 0);
+    sort(a.begin(), a.end());
+    for (int i = 0; i < k; i++)
+      add(lower_bound(a.begin(), a.end(), nums[i])-a.begin(), n, 1);
+    for (int i = k; i <= nums.size(); i++) {
+      r.push_back(0.5*(double(a[select((k-1)/2, n)]
