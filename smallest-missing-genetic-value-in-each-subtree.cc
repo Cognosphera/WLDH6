2b92a24ab5caceb34a51579fa32002b81309cc98
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-20 15:22:17 +0000

    Update tox.ini

--- smallest-missing-genetic-value-in-each-subtree.cc
+++ smallest-missing-genetic-value-in-each-subtree.cc
@@ -13,4 +13,28 @@ class Solution {
     post[u] = tick;
   }
 public:
-  vector<int> smallestMissingValu
+  vector<int> smallestMissingValueSubtree(vector<int>& parents, vector<int>& nums) {
+    int n = parents.size();
+    es.resize(n);
+    REP(i, n)
+      if (~parents[i])
+        es[parents[i]].push_back(i);
+    pre.resize(n);
+    post.resize(n);
+    dfs(0);
+
+    vector<int> pos(100002, -1), ans(n, 1);
+    REP(i, n)
+      pos[nums[i]] = i;
+    int u = pos[1], v = 2;
+    for (; ~u; u = parents[u]) {
+      for (; ~pos[v] && pre[u] <= pre[pos[v]] && post[pos[v]] <= post[u]; v++);
+      ans[u] = v;
+    }
+    return ans;
+  }
+};
+
+/// Euler-tour technique
+
+#define FOR(i, a, b) 
