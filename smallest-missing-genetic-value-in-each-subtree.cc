71d3a9ce616381256d0ffb85bd37c85d3ad5916c
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-21 22:36:06 +0000

    changed travis file

--- smallest-missing-genetic-value-in-each-subtree.cc
+++ smallest-missing-genetic-value-in-each-subtree.cc
@@ -37,4 +37,24 @@ public:
 
 /// Euler-tour technique
 
-#define FOR(i, a, b) 
+#define FOR(i, a, b) for (remove_cv<remove_reference<decltype(b)>::type>::type i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+
+class Solution {
+  vector<vector<int>> es;
+  vector<int> pre, post;
+  int tick = 0;
+  void dfs(int u) {
+    pre[u] = tick++;
+    for (int v : es[u])
+      dfs(v);
+    post[u] = tick;
+  }
+public:
+  vector<int> smallestMissingValueSubtree(vector<int>& parents, vector<int>& nums) {
+    int n = parents.size();
+    es.resize(n);
+    REP(i, n)
+      if (~parents[i])
+        es[parents[i]].push_back(i);
+    pre.resize(n)
