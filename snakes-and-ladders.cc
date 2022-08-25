beb9a84bbc4c58cbce3afdeb4cb269dc9266f209
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-25 12:50:43 +0000

    Synchrophasor: Updated openHistorian trending tool: HistorianView.

--- snakes-and-ladders.cc
+++ snakes-and-ladders.cc
@@ -7,4 +7,28 @@
 class Solution {
 public:
   int snakesAndLadders(vector<vector<int>>& a) {
-    int m = a.size(), n = a
+    int m = a.size(), n = a[0].size(), k = 0;
+    vector<int> b(m*n), d(m*n, -1);
+    deque<int> q;
+    ROF(i, 0, m)
+      if ((m-i-1)%2 == 0)
+        REP(j, n)
+          b[k++] = a[i][j]-1;
+      else
+        ROF(j, 0, n)
+          b[k++] = a[i][j]-1;
+    d[0] = 0;
+    for (q.push_back(0); q.size(); ) {
+      int x = q.front();
+      q.pop_front();
+      REP1(i, 6)
+        if (x+i < m*n) {
+          int y = x+i;
+          if (b[y] >= 0) y = b[y];
+          if (d[y] < 0) {
+            d[y] = d[x]+1;
+            q.push_back(y);
+          }
+        }
+    }
+    return d[m*n-1];
