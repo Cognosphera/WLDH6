8670c19b4372df683cca75804128d53cb931adfa
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-08-28 14:00:41 +0000

    dce

--- sort-colors.cc
+++ sort-colors.cc
@@ -21,4 +21,33 @@ public:
 class Solution {
 public:
   void sortColors(vector<int> &a) {
-    int w = 
+    int w = 0, r = 0, u = a.size(), b = a.size();
+    // -> wrbw
+    while (r < u)
+      if (a[r] == 0)
+        r++;
+      else if (a[r] == 1)
+        swap(a[w++], a[r++]);
+      else if (a[u-1] == 2)
+        u--;
+      else if (a[u-1] == 1)
+        swap(a[--b], a[--u]);
+      else
+        swap(a[r++], a[--u]);
+    // wrbw -> rwb
+    while (w)
+      swap(a[--w], a[--r]);
+    while (b < a.size())
+      swap(a[u++], a[b++]);
+  }
+};
+
+///
+
+class Solution {
+public:
+  void sortColors(vector<int> &a) {
+    int c[3] = {};
+    for (auto x: a)
+      c[x]++;
+    fill_n(a.begin(), c[0],
