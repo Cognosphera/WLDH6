ffd075bc4a16f5a025c121517d781207842097e0
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-09-06 20:57:51 +0000

    Update preview

--- strange-printer-ii.rs
+++ strange-printer-ii.rs
@@ -1,4 +1,20 @@
 // Strange Printer II
 impl Solution {
     pub fn is_printable(grid: Vec<Vec<i32>>) -> bool {
-        use std::collections::H
+        use std::collections::HashSet;
+        fn cycle(u: usize, vis: &mut Vec<usize>, adj: &Vec<HashSet<usize>>) -> bool {
+            vis[u] = 1;
+            if adj[u].iter().any(|&v| vis[v] == 1 || vis[v] == 0 && cycle(v, vis, adj)) {
+                return true;
+            }
+            vis[u] = 2;
+            false
+        }
+        let nc = 61;
+        let m = grid.len();
+        let n = grid[0].len();
+        let mut adj: Vec<HashSet<usize>> = vec![HashSet::new(); nc];
+        for c in 1..nc {
+            let mut x0 = m; let mut x1 = 0;
+            let mut y0 = n; let mut y1 = 0;
+    
