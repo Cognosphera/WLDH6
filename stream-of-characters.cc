41439d3aa433cf2bca2ca8ab48c095241c169d71
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-09-10 08:27:47 +0000

    add todos

--- stream-of-characters.cc
+++ stream-of-characters.cc
@@ -16,4 +16,24 @@ public:
             sz *= 2;
             auto pool2 = (Node*)realloc(pool, sizeof(Node)*sz);
             x = x-pool + pool2;
-            al
+            allo = allo-pool + pool2;
+            pool = pool2;
+          }
+          memset(allo, 0, sizeof(Node));
+          x->c[c-'a'] = allo++ - pool;
+        }
+        x = pool+x->c[c-'a'];
+      }
+      x->end = 1;
+    }
+    auto q = new int[allo-pool];
+    int qs = 0;
+    for (int i = 0; i < 26; i++)
+      if (pool->c[i])
+        q[qs++] = pool->c[i];
+    for (int i = 0; i < qs; i++) {
+      Node *x = pool+q[i];
+      x->end |= pool[x->pi].end;
+      for (int c = 0; c < 26; c++)
+        if (!x->c[c])
+        
