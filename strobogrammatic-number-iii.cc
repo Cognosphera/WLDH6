d1521f1693fc88dfbce777ed56a1e660ca863c5e
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-09-12 19:45:24 +0000

    v1.10

--- strobogrammatic-number-iii.cc
+++ strobogrammatic-number-iii.cc
@@ -7,4 +7,21 @@ class Solution {
     for (int i = 4; i < 27; i++)
       c[i] = c[i-2] * 5;
     c0[0] = 1; c0[1] = 3;
-    for (int i = 2; i < 27; i++
+    for (int i = 2; i < 27; i++)
+      c0[i] = c0[i-2] * 5;
+    int n = high.size(), m = (n+1)/2, s = accumulate(c+1, c+n, 0);
+    string pat(n, '0');
+    bool f = true;
+    for (int i = 0; i < m && f; i++) {
+      f = false;
+      for (auto q: qq) {
+        if (n % 2 && i == m-1 && q[0] != q[1])
+          continue;
+        if (q[0] != '0' || i) {
+          if (q[0] < high[i])
+            s += c0[max(0, n-(i+1)*2)];
+          if (q[0] == high[i]) {
+            f = true;
+            pat[i] = q[0];
+            pat[n-i-1] = q[1];
+       
