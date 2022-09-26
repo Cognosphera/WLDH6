886c145abfd676fd44d76e4c37893c8ab8fd404a
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-09-26 21:32:35 +0000

    f2fs: introduce a radix_tree for the free_nid list

--- sudoku-solver.cc
+++ sudoku-solver.cc
@@ -6,4 +6,26 @@ class Solution {
 private:
   static const int N = 9, MAXC = N*N*4, M = MAXC+1+N*N*N*4;
   int L[M], R[M], U[M], D[M], col[M], size[M], mean[M];
-  vo
+  void cover(int c) {
+    L[R[c]] = L[c];
+    R[L[c]] = R[c];
+    for (int i = D[c]; i != c; i = D[i])
+      for (int j = R[i]; j != i; j = R[j]) {
+        U[D[j]] = U[j];
+        D[U[j]] = D[j];
+      }
+  }
+  void uncover(int c) {
+    for (int i = U[c]; i != c; i = U[i])
+      for (int j = L[i]; j != i; j = L[j])
+        U[D[j]] = D[U[j]] = j;
+    L[R[c]] = R[L[c]] = c;
+  }
+  void append(int i, int l, int r, int c) {
+    L[i] = l; R[i] = r;
+    D[U[i] = U[c]] = i;
+    U[D[i] = c] = i;
+    col[i] = c;
+    size[c]++;
+  }
+  bool dlx(vector<vector<c
