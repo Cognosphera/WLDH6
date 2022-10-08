499ca219433978b524389818a53590bc469a5519
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-10-08 09:04:35 +0000

    Added makefile.

--- sudoku-solver.cc
+++ sudoku-solver.cc
@@ -28,4 +28,25 @@ private:
     col[i] = c;
     size[c]++;
   }
-  bool dlx(vector<vector<c
+  bool dlx(vector<vector<char> > &a) {
+    if (R[MAXC] == MAXC)
+      return true;
+    int c, mins = INT_MAX;
+    for (int i = R[MAXC]; i != MAXC; i = R[i])
+      if (size[i] < mins)
+        mins = size[c = i];
+    cover(c);
+    for (int i = D[c]; i != c; i = D[i]) {
+      for (int j = R[i]; j != i; j = R[j])
+        cover(col[j]);
+      a[mean[i]/N/N][mean[i]/N%N] = '1'+mean[i]%N;
+      if (dlx(a)) return true;
+      for (int j = L[i]; j != i; j = L[j])
+        uncover(col[j]);
+    }
+    uncover(c);
+    return false;
+  }
+public:
+  void solveSudoku(vector<vector<char> > &a) {
+    int cur = MAXC
