fb7f2f64ec89353128c2de08cda11c2fd5278d5f
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-11-05 13:45:22 +0000

    modified README.md

---
+++ the-employee-that-worked-on-the-longest-task.cc
@@ -0,0 +1,7 @@
+
+// The Employee That Worked on the Longest Task
+#define FOR(i, a, b) for (long i = (a); i < (b); i++)
+
+class Solution {
+public:
+  int hardestWorker(int n, vector<vector<int>>& logs) {
