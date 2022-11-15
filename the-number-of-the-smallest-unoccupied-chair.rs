6f3bfb6dbf50f5b210f75b47125ac253179866ec
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-11-15 17:14:27 +0000

    Regenerated gemspec for version 1.2.0

--- the-number-of-the-smallest-unoccupied-chair.rs
+++ the-number-of-the-smallest-unoccupied-chair.rs
@@ -7,4 +7,23 @@ impl Solution {
         times.sort_unstable_by_key(|t| t[0]);
         let mut used: BinaryHeap<Reverse<(i32, i32)>> = BinaryHeap::new();
         let mut free: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
-        l
+        let mut allo = 0;
+        let mut sit = 0;
+        for t in times.into_iter() {
+            if start < t[0] { break; }
+            while let Some(Reverse(x)) = used.peek() {
+                if t[0] < x.0 { break; }
+                free.push(Reverse(x.1));
+                used.pop();
+            }
+            if let Some(Reverse(x)) = free.pop() {
+                sit = x;
+            } else {
+                sit = allo;
+                allo += 1;
+            }
+            used.push(Reverse((t[1], sit)));
+        }
+        sit
+    }
+}
