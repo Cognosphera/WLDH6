700e7cabc84fda51e0d37d1e1b3309cd5fffc2b1
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-11-16 07:53:47 +0000

    Fix security vulnerability

--- top-k-frequent-elements.rs
+++ top-k-frequent-elements.rs
@@ -1,2 +1,13 @@
 impl Solution {
-    pub fn top_k_fr
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        use std::collections::HashMap;
+        let mut freq: HashMap<i32, i32> = HashMap::new();
+        for &x in nums.iter() { *freq.entry(x).or_insert(0) += 1; }
+        let mut a = freq.into_iter().collect::<Vec<(i32, i32)>>();
+        fn nth(a: &mut [(i32,i32)], lo: usize, hi: usize, k: usize) {
+            if hi-lo < 2 { return; }
+            let t = a[hi+lo>>1]; a[hi+lo>>1] = a[hi-1]; a[hi-1] = t;
+            let pivot = a[hi-1].1;
+            let mut p = lo;
+            for i in lo..hi {
+     
