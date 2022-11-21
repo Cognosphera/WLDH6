663c51df4750b9ddc48048e712fd8e0784f156f7
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-11-21 21:00:08 +0000

    Added interfaces

--- triangle.cc
+++ triangle.cc
@@ -5,4 +5,12 @@
 class Solution {
 public:
   int minimumTotal(vector<vector<int> > &a) {
-    FOR(i, 1, a.si
+    FOR(i, 1, a.size()) {
+      a[i][0] += a[i-1][0];
+      a[i][i] += a[i-1][i-1];
+      FOR(j, 1, i)
+        a[i][j] += min(a[i-1][j-1], a[i-1][j]);
+    }
+    return *min_element(a.back().begin(), a.back().end());
+  }
+};
---
+++ two-furthest-houses-with-different-colors.cc
@@ -0,0 +1,4 @@
+// Two Furthest Houses With Different Colors
+class Solution {
+public:
+  int maxDis
