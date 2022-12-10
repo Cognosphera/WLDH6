8c2490a47fdb27bfe566f52bd38de527197d7213
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-12-10 15:06:34 +0000

    mac80211: check management frame header length

--- validate-binary-search-tree.cc
+++ validate-binary-search-tree.cc
@@ -1,1 +1,31 @@
-/
+// Validate Binary Search Tree
+
+class Solution {
+public:
+  bool isValidBST(TreeNode *x) {
+    TreeNode *y = NULL;
+    stack<TreeNode *> s;
+    for(;;) {
+      for (; x; x = x->left)
+        s.push(x);
+      if (s.empty())
+        return true;
+      x = s.top();
+      s.pop();
+      if (y && x->val <= y->val)
+        return false;
+      y = x;
+      x = x->right;
+    }
+  }
+};
+
+///
+
+class Solution {
+public:
+  bool isValidBST(TreeNode *root) {
+    return f(root, NULL, NULL);
+  }
+  bool f(TreeNode *x, TreeNode *l, TreeNode *h) {
+    return !x || (!l || l->val
