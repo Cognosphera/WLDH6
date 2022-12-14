dfe567dc8fb955a4143fc47377eae680d3543370
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2022-12-14 19:09:16 +0000

    Fix a compile error.

--- validate-ip-address.cc
+++ validate-ip-address.cc
@@ -3,4 +3,25 @@ class Solution {
   bool ipv4(string a) {
     int p = 0, t;
     for (int j, i = 0; i < a.size(); i = j+1) {
-      for (j = i; j < a.size() && is
+      for (j = i; j < a.size() && isdigit(a[j]); j++);
+      if (i == j || j-i > 3 ||
+          j-i > 1 && a[i] == '0' ||
+          j < a.size() && a[j] != '.')
+        return false;
+      t = atoi(a.c_str()+i);
+      if (! (0 <= t && t < 256))
+        return false;
+      p++;
+    }
+    return a.size() && a.back() != '.' && p == 4;
+  }
+  bool ipv6(string a) {
+    int p = 0, t;
+    for (int j, i = 0; i < a.size(); i = j+1) {
+      for (j = i; j < a.size() && isxdigit(a[j]); j++);
+      if (i == j || j-i > 4 ||
+          j < a.size() && a[j] != ':')
+        return false;
+      p++;
+    }
+    return a.s
