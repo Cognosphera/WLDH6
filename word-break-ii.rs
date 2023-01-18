45f0251c9ccafca26b37fd1ce9f1280c8764bb2c
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2023-01-18 06:12:19 +0000

    Closes #16

--- word-break-ii.rs
+++ word-break-ii.rs
@@ -3,4 +3,18 @@ impl Solution {
         struct Sol<'a> { dict: &'a [String], ans: Vec<String>, parts: Vec<&'a str> }
         impl<'a> Sol<'a> {
             fn dfs(self: &mut Self, s: &str) {
-     
+                if s.is_empty() {
+                    self.ans.push(self.parts.join(" "));
+                    return;
+                }
+                for w in self.dict.iter() {
+                    if s.starts_with(w) {
+                        self.parts.push(w);
+                        self.dfs(&s[w.len()..]);
+                        self.parts.pop();
+                    }
+                }
+            }
+        }
+        let mut sol = Sol { dict: &word_dict, ans: vec![], parts: vec![] };
+        sol.df
