bdaac76c1b3f159cc093ba25062ce9f4f588e913
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2023-02-21 08:59:01 +0000

    Update schedule.yml

--- word-search-ii.cc
+++ word-search-ii.cc
@@ -12,4 +12,27 @@ class Solution {
     }
   } *root;
 
-  
+  int m, n;
+  vector<vector<char>> board;
+  vector<string> words, ans;
+
+public:
+  Solution() : root(new TrieNode) {}
+
+  vector<string> findWords(vector<vector<char>> &board, vector<string> &words) {
+    m = board.size();
+    n = m ? board[0].size() : 0;
+    REP(i, words.size()) {
+      auto x = root;
+      for (auto c: words[i]) {
+        if (! x->c[c-'a'])
+          x->c[c-'a'] = new TrieNode;
+        x = x->c[c-'a'];
+      }
+      x->id = i;
+    }
+    this->board = board;
+    this->words = words;
+    ans.clear();
+    REP(i, m)
+      REP
