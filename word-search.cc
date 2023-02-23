deeacd65eafb100e6418fa2ce8e637bd0c94e25b
Cognosphera <102710946+Cognosphera@users.noreply.github.com>
2023-02-23 14:53:35 +0000

    persist

--- word-search.cc
+++ word-search.cc
@@ -1,4 +1,25 @@
 // Word Search
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
-class S
+class Solution {
+private:
+  bool f(vector<vector<char> > &a, int x, int y, const char *p) {
+    if (a[x][y] != *p++) return false;
+    a[x][y] = ~a[x][y];
+    bool flag = false;
+    if (!*p)
+      flag = true;
+    else
+      REP(i, 4) {
+        int xx = x+(int[]){-1,0,1,0}[i], yy = y+(int[]){0,1,0,-1}[i];
+        if (unsigned(xx) < a.size() && unsigned(yy) < a[0].size() && f(a, xx, yy, p)) {
+          flag = true;
+          break;
+        }
+      }
+    a[x][y] = ~a[x][y];
+    return flag;
+  }
+public:
+  bool exist(vector<vector<char> > &board, string word) {
+    if (word.empty(
